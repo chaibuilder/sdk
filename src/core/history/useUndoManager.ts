385b27b08f022a462a07d89f19def98c48def5ba
Suraj Air <surajair@gmail.com>
2024-05-30 19:05:43 +0530

    feat: new undo manager implementation

---
+++ src/core/history/useUndoManager.ts
@@ -0,0 +1,20 @@
+import UndoManager from "undo-manager";
+import { useEffect } from "react";
+import { noop } from "lodash-es";
+
+const undoManager = new UndoManager();
+undoManager.setLimit(10);
+
+const useUndoManager = () => {
+  useEffect(() => {
+    undoManager.setCallback(() => {
+      console.log("undoManager changed");
+    });
+    return () => {
+      undoManager.setCallback(noop);
+    };
+  }, []);
+  return { undoManager, hasRedo: undoManager.hasRedo(), hasUndo: undoManager.hasUndo(), reset: undoManager.reset() };
+};
+
+export { useUndoManager };
