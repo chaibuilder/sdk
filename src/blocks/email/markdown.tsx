adb99e718712d65c9626916fa6c82bd02b811392
Suraj Air <surajair@gmail.com>
2024-06-04 21:32:53 +0530

    feat: added react email blocks with tailwind support

---
+++ src/blocks/email/markdown.tsx
@@ -0,0 +1,17 @@
+import { Markdown } from "@react-email/components";
+import { registerChaiBlock } from "@chaibuilder/runtime";
+import { MultilineText } from "@chaibuilder/runtime/controls";
+
+const MarkdownBlock = ({ blockProps, content }) => {
+  return <Markdown children={content} {...blockProps}></Markdown>;
+};
+
+registerChaiBlock(MarkdownBlock, {
+  type: "Markdown",
+  label: "Markdown",
+  group: "basic",
+  category: "core",
+  props: {
+    content: MultilineText({ default: "# This is a ~~strikethrough~~", title: "Markdown" }),
+  },
+});
--- src/core/components/canvas/static/StaticBlocksRenderer.tsx
+++ src/core/components/canvas/static/StaticBlocksRenderer.tsx
@@ -1,18 +1,22 @@
 import { filter, isEmpty } from "lodash-es";
 import { Provider } from "react-wrap-balancer";
-import { useAllBlocks } from "../../../hooks";
+import { useAllBlocks, useBuilderProp } from "../../../hooks";
 import { BlocksRendererStatic } from "./BlocksRenderer";
 import { BlocksExternalDataProvider } from "./BlocksExternalDataProvider.tsx";
+import { getBlockComponent } from "@chaibuilder/runtime";
+import { createElement } from "react";
 
 export const StaticBlocksRenderer = () => {
   const blocks = useAllBlocks();
-  return (
-    <Provider>
-      {isEmpty(blocks) ? null : (
-        <BlocksExternalDataProvider>
-          <BlocksRendererStatic blocks={filter(blocks, (block) => isEmpty(block._parent))} />
-        </BlocksExternalDataProvider>
-      )}
-    </Provider>
+  const container = useBuilderProp("container", null);
+
+  const blocksHtml = isEmpty(blocks) ? null : (
+    <BlocksExternalDataProvider>
+      <BlocksRendererStatic blocks={filter(blocks, (block) => isEmpty(block._parent))} />
+    </BlocksExternalDataProvider>
   );
+
+  const children = container ? createElement(getBlockComponent(container).component, {}, blocksHtml) : blocksHtml;
+
+  return <Provider>{children}</Provider>;
 };
