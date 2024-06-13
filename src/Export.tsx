2c624a557ac0e74a991fcaff7b20a008206f3213
Rahul <rajamohanty12345@gmail.com>
2024-06-13 15:44:07 +0530

    feat: moved copy and download button to bottom

--- src/Export.tsx
+++ src/Export.tsx
@@ -1,11 +1,8 @@
 import React, { useState, useCallback } from "react";
-import { ClipboardIcon, DownloadIcon } from "@radix-ui/react-icons";
 import { useCopyToClipboard } from "./core/hooks/useCopyToClipboard";
 import { ErrorBoundary } from "./core/components/ErrorBoundary";
 import { Button, Dialog, DialogContent, DialogFooter, DialogTrigger } from "./ui";
-import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/radix/components/ui/tooltip";
 import { CodeBlock, oneLight } from "@react-email/components";
-import { DialogClose } from "@radix-ui/react-dialog";
 import { useTranslation } from "react-i18next";
 
 interface ExportModalProps {
@@ -15,7 +12,7 @@ interface ExportModalProps {
 
 const ExportModal: React.FC<ExportModalProps> = React.memo(({ content, handleClick }) => {
   const [emailHTMLContent, setEmailHTMLContent] = useState<string>("");
-  const [copiedText, copy] = useCopyToClipboard();
+  const [, copy] = useCopyToClipboard();
   const [isCopied, setIsCopied] = useState<boolean>(false);
   const { t } = useTranslation();
 
@@ -24,14 +21,14 @@ const ExportModal: React.FC<ExportModalProps> = React.memo(({ content, handleCli
       copy(text)
         .then(() => {
           setIsCopied(true);
-          console.log("Copied!", { copiedText, text });
+          console.log("Copied!",  isCopied);
         })
         .catch((error) => {
           setIsCopied(false);
           console.error("Failed to copy!", error);
         });
     },
-    [copy, copiedText],
+    [copy, isCopied],
   );
 
   const downloadHTMLContent = (htmlContent: string) => {
@@ -58,43 +55,16 @@ const ExportModal: React.FC<ExportModalProps> = React.memo(({ content, handleCli
       <DialogContent className="flex max-h-[80vh] max-w-xl flex-col gap-0 overflow-auto p-0 lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl">
         <ErrorBoundary>
           <div className="sticky top-0 z-10 flex items-center justify-between bg-white px-4 pt-3.5 text-lg font-bold">
-            HTML
-            <div className="flex items-center gap-2">
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <DownloadIcon
-                    onClick={() => downloadHTMLContent(emailHTMLContent)}
-                    className={`size-6 cursor-pointer rounded-md border p-1 transition-all ease-in-out`}
-                  />
-                </TooltipTrigger>
-                <TooltipContent side="left">
-                  <p className="text-sm tracking-wide">Download</p>
-                </TooltipContent>
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <ClipboardIcon
-                    onClick={handleCopy(emailHTMLContent)}
-                    className={`size-6 cursor-pointer rounded-md border p-1 transition-all ease-in-out`}
-                  />
-                </TooltipTrigger>
-                <TooltipContent side="left">
-                  <p className="text-sm tracking-wide">{`${!isCopied ? "Copy" : "Copied"} to clipboard`}</p>
-                </TooltipContent>
-              </Tooltip>
-            </div>
+            Export Email Template
           </div>
-
           <CodeBlock code={emailHTMLContent} language="html" theme={oneLight} />
           <DialogFooter className="flex px-4 pb-2">
-            <DialogClose asChild>
-              <Button type="button" variant="secondary">
-                {t("cancel")}
-              </Button>
-            </DialogClose>
-            <DialogClose asChild>
-              <Button type="button">{t("save")}</Button>
-            </DialogClose>
+            <Button type="button" onClick={() => downloadHTMLContent(emailHTMLContent)}>
+              {t("Download")}
+            </Button>
+            <Button type="button" variant="outline" onClick={handleCopy(emailHTMLContent)}>
+              {t("Copy")}
+            </Button>
           </DialogFooter>
         </ErrorBoundary>
       </DialogContent>
