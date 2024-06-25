f49eb98f44eb38eab633211b8cafff955f28519a
Rahul <rajamohanty12345@gmail.com>
2024-06-25 22:41:41 +0530

    feat: fixed the additional blocks as child by removing duplicates

--- src/core/components/sidepanels/panels/outline/arborist/ListTree.tsx
+++ src/core/components/sidepanels/panels/outline/arborist/ListTree.tsx
@@ -1,48 +1,137 @@
 import { Tree } from "react-arborist";
 import { cn } from "../../../../../functions/Functions.ts";
-import { useSelectedBlockIds, useSelectedStylingBlocks } from "../../../../../hooks";
-import { ScrollArea } from "../../../../../../ui";
+import {
+  useBuilderProp,
+  useSelectedBlockIds,
+  useSelectedStylingBlocks,
+  useHighlightBlockId,
+  useBlocksStore,
+} from "../../../../../hooks";
+import { ScrollArea, Tooltip, TooltipContent, TooltipTrigger } from "../../../../../../ui";
+import { useCallback, useMemo } from "react";
 
-const data = [
-  { id: "1", name: "Unread" },
-  { id: "2", name: "Threads" },
-  {
-    id: "3",
-    name: "Chat Rooms",
-    children: [
-      { id: "c1", name: "General" },
-      { id: "c2", name: "Random" },
-      { id: "c3", name: "Open Source Projects" },
-    ],
-  },
-  {
-    id: "4",
-    name: "Direct Messages",
-    children: [
-      { id: "d1", name: "Alice" },
-      { id: "d2", name: "Bob" },
-      { id: "d3", name: "Charlie" },
-    ],
-  },
-];
+import { getBlocksTree } from "../../../../../functions/Blocks.ts";
+import { TypeIcon } from "../TypeIcon.tsx";
+import { TriangleRightIcon } from "@radix-ui/react-icons";
+
+const removeDuplicates = (nodes, seen = new Set()) => {
+  return nodes.reduce((acc, node) => {
+    if (!seen.has(node._id)) {
+      seen.add(node._id);
+      const children = node.children ? removeDuplicates(node.children, seen) : [];
+      acc.push({ ...node, children });
+    }
+    return acc;
+  }, []);
+};
+
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
 
 function Node({ node, style, dragHandle }) {
-  /* This node instance can do many things. See the API reference. */
+  const [, setHighlighted] = useHighlightBlockId();
+  const outlineItems = useBuilderProp("outlineMenuItems", []);
+  
+  const { id, isSelected, data, handleClick,  } = node;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSetHighlighted = useCallback(debounce(setHighlighted, 1000), []);
+
+  const handleToggle = (event: any) => {
+    event.stopPropagation();
+    /**
+     * Toggle the node open and close State
+     */
+    node.toggle();
+  };
+
+  /**
+   * To stop propagation of the event to the parent
+   * Tree Component to avoid clearing the selection of blocks
+   * and allowing to select other blocks
+   */
+  const handleClearSelection = (e) => {
+    e.stopPropagation();
+    /**
+     * It will work when a node is clicked.
+     * The onSelect in the parent Tree Component
+     * will also trigger the selection of the node.
+     */
+    handleClick(e);
+  };
+
   return (
-    <div style={style} ref={dragHandle}>
-      {node.isLeaf ? "🍁" : "🗀"}
-      {node.data.name}
+    <div
+      onClick={handleClearSelection}
+      key={id}
+      onMouseEnter={() => debouncedSetHighlighted(id)}
+      style={style}
+      ref={dragHandle}
+      className={cn(
+        "group flex !h-fit w-full items-center justify-between space-x-px py-px",
+         isSelected ? "bg-blue-500 text-white" : "text-gray-600 hover:bg-gray-200 dark:hover:bg-gray-800",
+      )}>
+      <div className="flex items-center">
+        <div
+          className={`flex h-4 w-4 rotate-0 transform cursor-pointer items-center justify-center text-xs transition-transform duration-100 ${
+            node.isOpen ? "rotate-90" : ""
+          }`}>
+          {node.children.length > 0 && (
+            <button onClick={handleToggle} type="button">
+              <TriangleRightIcon />
+            </button>
+          )}
+        </div>
+        <button type="button" className="flex items-center">
+          <div className="-mt-1 h-3 w-3">
+            <TypeIcon type={data?._type} />
+          </div>
+          <div className="ml-2 truncate text-[11px]">{data?._type}</div>
+        </button>
+      </div>
+      <div className="invisible flex items-center space-x-1 pr-2 group-hover:visible">
+        {outlineItems.map((outlineItem) => (
+          <Tooltip>
+            <TooltipTrigger
+              className="cursor-pointer rounded bg-transparent hover:bg-white hover:text-blue-500"
+              asChild>
+              {outlineItem.item(id)}
+            </TooltipTrigger>
+            <TooltipContent className="">{outlineItem.tooltip}</TooltipContent>
+          </Tooltip>
+        ))}
+      </div>
     </div>
   );
 }
 
 const ListTree = () => {
+  const [allBlocks] = useBlocksStore();
+
   const [ids, setIds] = useSelectedBlockIds();
   const [, setStyleBlocks] = useSelectedStylingBlocks();
+
   const clearSelection = () => {
     setIds([]);
     setStyleBlocks([]);
   };
+
+  const treeData = useMemo(() => {
+    let treeBlocks = getBlocksTree(allBlocks);
+    /***
+     * Removing the duplicates from the TreeBlocks
+     */
+    treeBlocks = removeDuplicates(treeBlocks);
+
+    return treeBlocks;
+  }, [allBlocks]);
 
   const onRename = ({ id, name }) => {
     console.log("onRename", { id, name });
@@ -54,17 +143,29 @@ const ListTree = () => {
     console.log("onDelete", { ids });
   };
 
+  const onSelect = (node: any) => {
+    const nodeId = node[0] ? node[0].id : "";
+    setStyleBlocks([]);
+    setIds([nodeId]);
+  };
+
+  
   return (
-    <div onClick={() => clearSelection()} className={cn("-mx-1 -mt-1 flex h-full select-none flex-col space-y-1")}>
+    <div className={cn("-mx-1 -mt-1 flex h-full select-none flex-col space-y-1")} onClick={() => clearSelection()}>
       <ScrollArea id="layers-view" className="no-scrollbar h-full overflow-y-auto p-1 px-2 text-xs">
         <Tree
           selection={ids[0] || ""}
           onRename={onRename}
+          openByDefault={false}
           onMove={onMove}
+          rowHeight={20}
           onDelete={onDelete}
-          data={data}
-          idAccessor={"id"}
-          childrenAccessor={(d) => d.children}>
+          data={treeData}
+          width={216}
+          indent={18}
+          idAccessor={"_id"}
+          onSelect={onSelect}
+          childrenAccessor={(d: any) => d.children}>
           {Node as any}
         </Tree>
       </ScrollArea>
