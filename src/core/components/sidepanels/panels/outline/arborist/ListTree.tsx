83c0de0740055cf855f6c3c97dca664e0e312c2b
Rahul Ku Mohanty <rajamohanty12345@gmail.com>
2024-07-01 12:23:44 +0530

    Fixed the Context Menu and Implemented CanDropBlock (#24)
    * chore: improved performance on editing text fields in settings
    
    * feat: implemented the Input UI on doubleclick
    
    * feat: implemented the Input UI on doubleclick
    
    * feat: included MoveHandler and RenameHandler types and rename node functionality
    
    ---------
    
    Co-authored-by: Suraj Air <surajair@gmail.com>

--- src/core/components/sidepanels/panels/outline/arborist/ListTree.tsx
+++ src/core/components/sidepanels/panels/outline/arborist/ListTree.tsx
@@ -1,33 +1,34 @@
-import { memo, useEffect } from "react";
+import { memo, useEffect, MouseEvent } from "react";
 import { useAtom } from "jotai";
 import { useDebouncedCallback } from "@react-hookz/web";
-import { NodeRendererProps, Tree } from "react-arborist";
-
+import { MoveHandler, NodeRendererProps, RenameHandler, Tree } from "react-arborist";
 import { treeDSBlocks } from "../../../../../atoms/blocks.ts";
-
 import { cn } from "../../../../../functions/Functions.ts";
-
 import {
   useBuilderProp,
   useHighlightBlockId,
   useSelectedBlockIds,
   useSelectedStylingBlocks,
+  useUpdateBlocksProps,
 } from "../../../../../hooks";
-
 import { TriangleRightIcon } from "@radix-ui/react-icons";
 import { ScrollArea, Tooltip, TooltipContent, TooltipTrigger } from "../../../../../../ui";
-
 import { TypeIcon } from "../TypeIcon.tsx";
 import { DefaultCursor } from "./DefaultCursor.tsx";
-import { useBlocksStoreUndoableActions } from "../../../../../history/useBlocksStoreUndoableActions.ts";
 import { DefaultDragPreview } from "./DefaultDragPreview.tsx";
+import { useBlocksStoreUndoableActions } from "../../../../../history/useBlocksStoreUndoableActions.ts";
+import { BlockContextMenu } from "../BlockContextMenu.tsx";
+import { canDropBlock } from "../../../../../functions/block-helpers.ts";
 
-const Node = memo(({ node, style, dragHandle }: Omit<NodeRendererProps<any>, "tree">) => {
-  const [, setHighlighted] = useHighlightBlockId();
+const Node = memo(({ node, style, dragHandle, tree }: NodeRendererProps<any>) => {
   const outlineItems = useBuilderProp("outlineMenuItems", []);
+  const [, setHighlighted] = useHighlightBlockId();
 
-  const isDropZone = node.children.length > 0;
-  const { id, isSelected, data, handleClick, willReceiveDrop, isDragging } = node;
+  const isDropAllowed = tree.props.disableDrop;
+
+  const hasChildren = node.children.length > 0;
+
+  const { id, data, isSelected, willReceiveDrop, isDragging, isEditing, handleClick } = node;
 
   const debouncedSetHighlighted = useDebouncedCallback((id) => setHighlighted(id), [], 300);
 
@@ -54,65 +55,95 @@ const Node = memo(({ node, style, dragHandle }: Omit<NodeRendererProps<any>, "tr
 
   useEffect(() => {
     const timedToggle = setTimeout(() => {
-      if (willReceiveDrop && isDropZone && !node.isOpen) {
+      if (willReceiveDrop && isDropAllowed && !node.isOpen) {
         node.toggle();
       }
     }, 500);
 
     return () => clearTimeout(timedToggle);
-  }, [willReceiveDrop, isDropZone, node]);
+  }, [willReceiveDrop, isDropAllowed, node]);
 
   return (
-    <div
-      onClick={handleNodeClickWithoutPropagating}
-      key={id}
-      onMouseEnter={() => debouncedSetHighlighted(id)}
-      style={style}
-      ref={dragHandle}
-      className={cn(
-        "group flex !h-fit w-full items-center justify-between space-x-px py-px",
-        isSelected ? "bg-blue-500 text-white" : "text-gray-600 hover:bg-gray-200 dark:hover:bg-gray-800",
-        willReceiveDrop && isDropZone && "bg-gray-200 text-gray-600",
-        isDragging && "opacity-20",
-      )}>
-      <div className="flex items-center">
-        <div
-          className={`flex h-4 w-4 rotate-0 transform cursor-pointer items-center justify-center text-xs transition-transform duration-100 ${
-            node.isOpen ? "rotate-90" : ""
-          }`}>
-          {isDropZone && (
-            <button onClick={handleToggle} type="button">
-              <TriangleRightIcon />
-            </button>
-          )}
-        </div>
-        <button type="button" className="flex items-center">
-          <div className="-mt-0.5 h-3 w-3">
-            <TypeIcon type={data?._type} />
+    <BlockContextMenu id={id}>
+      <div
+        onClick={handleNodeClickWithoutPropagating}
+        onMouseEnter={() => debouncedSetHighlighted(id)}
+        style={style}
+        data-node-id={id}
+        ref={dragHandle}
+        className={cn(
+          "group flex !h-fit w-full items-center justify-between space-x-px py-px",
+          isSelected ? "bg-blue-500 text-white" : "text-gray-600 hover:bg-gray-200 dark:hover:bg-gray-800",
+          isDropAllowed && willReceiveDrop && "bg-green-200 text-green-600",
+          isDragging && "opacity-20",
+        )}>
+        <div className="flex items-center">
+          <div
+            className={`flex h-4 w-4 rotate-0 transform cursor-pointer items-center justify-center text-xs transition-transform duration-100 ${
+              node.isOpen ? "rotate-90" : ""
+            }`}>
+            {hasChildren && (
+              <button onClick={handleToggle} type="button">
+                <TriangleRightIcon />
+              </button>
+            )}
           </div>
-          <div className="ml-2 truncate text-[11px]">{data?._type}</div>
-        </button>
+          <button type="button" className="flex items-center">
+            <div className="-mt-0.5 h-3 w-3">
+              <TypeIcon type={data?._type} />
+            </div>
+            {isEditing ? (
+              <Input node={node} />
+            ) : (
+              <div
+                className="ml-2 truncate text-[11px]"
+                onDoubleClick={(e) => { 
+                  e.stopPropagation();
+                  node.edit();
+                  node.deselect();
+                }}>
+                {data?._name || data?._type}
+              </div>
+            )}
+          </button>
+        </div>
+        <div className="invisible flex items-center space-x-1 pr-2 group-hover:visible">
+          {outlineItems.map((outlineItem) => (
+            <Tooltip>
+              <TooltipTrigger
+                className="cursor-pointer rounded bg-transparent hover:bg-white hover:text-blue-500"
+                asChild>
+                {outlineItem.item(id)}
+              </TooltipTrigger>
+              <TooltipContent className="z-[9999]">{outlineItem.tooltip}</TooltipContent>
+            </Tooltip>
+          ))}
+        </div>
       </div>
-      <div className="invisible flex items-center space-x-1 pr-2 group-hover:visible">
-        {outlineItems.map((outlineItem) => (
-          <Tooltip>
-            <TooltipTrigger
-              className="cursor-pointer rounded bg-transparent hover:bg-white hover:text-blue-500"
-              asChild>
-              {outlineItem.item(id)}
-            </TooltipTrigger>
-            <TooltipContent className="z-[9999]">{outlineItem.tooltip}</TooltipContent>
-          </Tooltip>
-        ))}
-      </div>
-    </div>
+    </BlockContextMenu>
   );
 });
 
+const Input = ({ node }) => {
+  return (
+    <input
+      autoFocus
+      className="ml-2 w-full rounded-sm border border-black/30 bg-transparent px-1 text-[11px] outline-none"
+      type="text"
+      defaultValue={node.data.name}
+      onFocus={(e) => e.currentTarget.select()}
+      onBlur={(e) => node.submit(e.currentTarget.value)}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") node.submit(e.currentTarget.value);
+      }}
+    />
+  );
+};
+
 const ListTree = () => {
   const [treeData] = useAtom(treeDSBlocks);
-
   const [ids, setIds] = useSelectedBlockIds();
+  const updateBlockProps = useUpdateBlocksProps();
   const [, setStyleBlocks] = useSelectedStylingBlocks();
   const { moveBlocks } = useBlocksStoreUndoableActions();
 
@@ -121,14 +152,11 @@ const ListTree = () => {
     setStyleBlocks([]);
   };
 
-  const onRename = ({ id, name }) => {
-    console.log("onRename", { id, name });
+  const onRename: RenameHandler<any> = ({ id, name, node }) => {
+    updateBlockProps([id], { _name: name }, node.data._name);
   };
-  const onMove = ({ dragIds, parentId, index }) => {
+  const onMove: MoveHandler<any> = ({ dragIds, parentId, index }) => {
     moveBlocks(dragIds, parentId, index);
-  };
-  const onDelete = ({ ids }) => {
-    console.log("onDelete", { ids });
   };
 
   const onSelect = (nodes: any) => {
@@ -136,6 +164,17 @@ const ListTree = () => {
     const nodeId = nodes[0] ? nodes[0].id : "";
     setStyleBlocks([]);
     setIds([nodeId]);
+  };
+
+  const onContextMenu = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const target = e.target as HTMLDivElement;
+    const nodeId =
+      target.getAttribute("data-node-id") || target.closest("[data-node-id]")?.getAttribute("data-node-id");
+    if (nodeId) {
+      setStyleBlocks([]);
+      setIds([nodeId]);
+    }
   };
 
   return (
@@ -148,7 +187,6 @@ const ListTree = () => {
           openByDefault={false}
           onMove={onMove}
           rowHeight={20}
-          onDelete={onDelete}
           data={treeData}
           renderCursor={DefaultCursor}
           onSelect={onSelect}
@@ -156,6 +194,13 @@ const ListTree = () => {
           width={"100%"}
           renderDragPreview={DefaultDragPreview}
           indent={10}
+          onContextMenu={onContextMenu}
+          disableDrop={({ parentNode, dragNodes }) =>
+            !canDropBlock(null, {
+              dragSource: dragNodes[0],
+              dropTarget: parentNode,
+            })
+          }
           idAccessor={"_id"}>
           {Node as any}
         </Tree>
