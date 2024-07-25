8891e3b8fa4d6ec21dad25811cdc370f889e4d68
Rahul <rajamohanty12345@gmail.com>
2024-07-25 14:48:42 +0530

    fix: removed the tab if one of the upload callback is missing

--- src/core/components/sidepanels/panels/images/ImagesPanel.tsx
+++ src/core/components/sidepanels/panels/images/ImagesPanel.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../../../ui";
 import { useBuilderProp } from "../../../../hooks";
-import clsx from "clsx";
 
 const UnsplashImages = React.lazy(() => import("./UnsplashImages"));
 const UploadImages = React.lazy(() => import("./UploadImages"));
@@ -13,33 +12,38 @@ const ImagesPanel = ({
   isModalView?: boolean;
   onSelect?: (url: string) => void;
 }): React.JSX.Element => {
-  const [tab, setTab] = useState("upload");
-
   const uploadImageCallback = useBuilderProp("uploadMediaCallback");
   const unsplashImageCallback = useBuilderProp("unsplashAccessKey");
 
   const missingUploadImageCallback = uploadImageCallback === undefined;
-  const missingUnslashImageCallback = unsplashImageCallback === undefined;
+  const missingUnsplashImageCallback = unsplashImageCallback === undefined;
 
+  const [tab, setTab] = useState(
+    missingUnsplashImageCallback ? "upload" : missingUploadImageCallback ? "unsplash" : "upload",
+  );
   return (
     <div className="flex h-full flex-col">
       <div className="flex items-center justify-between rounded-md bg-background/30 p-1">
-        <h1 className="px-1 font-semibold">{isModalView ? "Select or upload images" : "Images"}</h1>
+        <h1 className="px-1 font-semibold">
+          {isModalView && missingUploadImageCallback
+            ? "Unsplash images"
+            : isModalView
+              ? "Select or upload images"
+              : "Images"}
+        </h1>
       </div>
 
       <Tabs value={tab} onValueChange={(v) => setTab(v as any)} className="flex h-full w-full flex-col py-2">
-        <TabsList className="w-full">
-          <TabsTrigger
-            value="upload"
-            className={clsx(missingUploadImageCallback && "pointer-events-none opacity-50", "w-1/2")}>
-            Upload
-          </TabsTrigger>
-          <TabsTrigger
-            value="unsplash"
-            className={clsx(missingUnslashImageCallback && "pointer-events-none opacity-50", "w-1/2")}>
-            Unsplash
-          </TabsTrigger>
-        </TabsList>
+        {!(missingUploadImageCallback || missingUnsplashImageCallback) && (
+          <TabsList className="w-full">
+            <TabsTrigger value="upload" className="w-full">
+              Upload
+            </TabsTrigger>
+            <TabsTrigger value="unsplash" className="w-full">
+              Unsplash
+            </TabsTrigger>
+          </TabsList>
+        )}
         {tab === "unsplash" ? (
           <TabsContent value="unsplash" className="flex h-full flex-col overflow-hidden">
             <Suspense fallback={<div className="h-64 w-full animate-pulse bg-gray-100" />}>
