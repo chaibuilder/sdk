49899854d76d430b9188a938c720117c9254891e
Rahul <rajamohanty12345@gmail.com>
2024-07-25 12:57:59 +0530

    fix: hidden replace button if upload Image callback is missing

--- src/core/components/sidepanels/panels/images/ImagesPanel.tsx
+++ src/core/components/sidepanels/panels/images/ImagesPanel.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../../../ui";
+import { useBuilderProp } from "../../../../hooks";
+import clsx from "clsx";
 
 const UnsplashImages = React.lazy(() => import("./UnsplashImages"));
 const UploadImages = React.lazy(() => import("./UploadImages"));
@@ -12,6 +14,13 @@ const ImagesPanel = ({
   onSelect?: (url: string) => void;
 }): React.JSX.Element => {
   const [tab, setTab] = useState("upload");
+
+  const uploadImageCallback = useBuilderProp("uploadMediaCallback");
+  const unsplashImageCallback = useBuilderProp("unsplashAccessKey");
+
+  const missingUploadImageCallback = uploadImageCallback === undefined;
+  const missingUnslashImageCallback = unsplashImageCallback === undefined;
+
   return (
     <div className="flex h-full flex-col">
       <div className="flex items-center justify-between rounded-md bg-background/30 p-1">
@@ -20,10 +29,14 @@ const ImagesPanel = ({
 
       <Tabs value={tab} onValueChange={(v) => setTab(v as any)} className="flex h-full w-full flex-col py-2">
         <TabsList className="w-full">
-          <TabsTrigger value="upload" className="w-1/2">
+          <TabsTrigger
+            value="upload"
+            className={clsx(missingUploadImageCallback && "pointer-events-none opacity-50", "w-1/2")}>
             Upload
           </TabsTrigger>
-          <TabsTrigger value="unsplash" className="w-1/2">
+          <TabsTrigger
+            value="unsplash"
+            className={clsx(missingUnslashImageCallback && "pointer-events-none opacity-50", "w-1/2")}>
             Unsplash
           </TabsTrigger>
         </TabsList>
