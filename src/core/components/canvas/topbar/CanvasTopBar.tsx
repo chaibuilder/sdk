ab79e07dd08e7d07f203ff3458fee7257c332ae6
Suraj Air <surajair@gmail.com>
2024-08-17 12:11:09 +0530

    feat: dark mode enabled by default

--- src/core/components/canvas/topbar/CanvasTopBar.tsx
+++ src/core/components/canvas/topbar/CanvasTopBar.tsx
@@ -10,20 +10,20 @@ import { useBuilderProp, useCanvasZoom } from "../../../hooks";
 import { AiAssistant } from "./AiAssistant.tsx";
 
 const CanvasTopBar: React.FC = () => {
-  const darkModeSupport = useBuilderProp("darkMode", false);
+  const darkModeSupport = useBuilderProp("darkMode", true);
   const [zoom] = useCanvasZoom();
 
   return (
     <div className="flex h-10 items-center justify-between border-b bg-background/70 px-2">
       <div className="flex h-full space-x-2">
+        <Breakpoints />
+        <Separator orientation="vertical" />
         {darkModeSupport ? (
           <>
             <DarkMode />
             <Separator orientation="vertical" />
           </>
         ) : null}
-        <Breakpoints />
-        <Separator orientation="vertical" />
         <div className="flex w-12 cursor-not-allowed items-center justify-center gap-x-1 space-x-0 font-medium text-gray-400">
           <ZoomInIcon className="h-3.5 w-3.5 flex-shrink-0" />{" "}
           <div className="text-xs leading-3">{round(zoom, 0)}%</div>
--- src/core/components/canvas/topbar/DarkMode.tsx
+++ src/core/components/canvas/topbar/DarkMode.tsx
@@ -1,29 +1,25 @@
-import { useTranslation } from "react-i18next";
 import { useDarkMode } from "../../../hooks";
-import { Label, Switch } from "../../../../ui";
+import { Switch } from "../../../../ui";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 export function DarkMode() {
   const [darkMode, setDarkMode] = useDarkMode();
-  const { t } = useTranslation();
-
   return (
     <div className="flex items-center">
-      <Label htmlFor="dark-mode-switch">{t("dark_mode")}</Label>
+      <SunIcon className="size-4 shrink-0" />
       <Switch
         id="dark-mode-switch"
         checked={darkMode}
         onCheckedChange={() => {
           setDarkMode(!darkMode);
         }}
-        className={`${darkMode ? "bg-violet-600" : "bg-violet-300"}
-          relative ml-2 inline-flex h-[20px] w-[40px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}>
-        <span className="sr-only">{t("use_setting")}</span>
+        className={`${darkMode ? "bg-violet-600" : "bg-violet-300"} relative ml-2 inline-flex h-[20px] w-[40px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}>
         <span
           aria-hidden="true"
-          className={`${darkMode ? "translate-x-5" : "translate-x-0"}
-            pointer-events-none -mt-px inline-block h-[18px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+          className={`${darkMode ? "translate-x-5" : "translate-x-0"} pointer-events-none -mt-px inline-block h-[18px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
         />
       </Switch>
+      <MoonIcon className="size-4 ml-3 shrink-0" />
     </div>
   );
 }
