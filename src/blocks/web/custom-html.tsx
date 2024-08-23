13a8a7b2aeed8f19319eecac79295e02467b2197
Suraj Air <surajair@gmail.com>
2024-08-23 09:00:26 +0530

    fix: removed additional wrapper div at render time

--- src/blocks/web/custom-html.tsx
+++ src/blocks/web/custom-html.tsx
@@ -4,27 +4,29 @@ import { registerChaiBlock } from "@chaibuilder/runtime";
 import { Code, Styles } from "@chaibuilder/runtime/controls";
 import { ChaiBlock } from "../../core/types/ChaiBlock.ts";
 
-const CustomHTMLBlock = React.memo(
-  (
-    props: ChaiBlock & {
-      blockProps: Record<string, string>;
-      styles: Record<string, string>;
-    },
-  ) => {
-    const { blockProps, styles, htmlCode, inBuilder } = props;
-    //remove all script tags from htmlCode
-    const htmlCodeWithoutScriptTags = htmlCode.replace(/<script.*?>.*?<\/script>/g, "");
-    return (
-      <div className={"relative"}>
-        {inBuilder ? <div {...blockProps} {...styles} className="absolute z-20 h-full w-full" /> : null}
-        {React.createElement("div", {
-          ...styles,
-          dangerouslySetInnerHTML: { __html: inBuilder ? htmlCodeWithoutScriptTags : htmlCode },
-        })}
-      </div>
-    );
+const CustomHTMLBlock = (
+  props: ChaiBlock & {
+    blockProps: Record<string, string>;
+    styles: Record<string, string>;
   },
-);
+) => {
+  const { blockProps, styles, htmlCode, inBuilder } = props;
+  return inBuilder ? (
+    <div className={"relative"}>
+      {inBuilder ? <div {...blockProps} {...styles} className="absolute z-20 h-full w-full" /> : null}
+      {React.createElement("div", {
+        ...styles,
+        dangerouslySetInnerHTML: { __html: htmlCode.replace(/<script.*?>.*?<\/script>/g, "") },
+      })}
+    </div>
+  ) : (
+    React.createElement("div", {
+      ...blockProps,
+      ...styles,
+      dangerouslySetInnerHTML: { __html: htmlCode },
+    })
+  );
+};
 
 registerChaiBlock(CustomHTMLBlock, {
   type: "CustomHTML",
@@ -43,4 +45,4 @@ registerChaiBlock(CustomHTMLBlock, {
   canAcceptBlock: () => false,
 });
 
-export default CustomHTMLBlock;
+export { CustomHTMLBlock };
