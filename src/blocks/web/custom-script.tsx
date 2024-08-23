6f5c2fe29a6c21ffe5a985cf109159b440083476
Suraj Air <surajair@gmail.com>
2024-08-23 16:43:38 +0530

    feat: executing scripts in preview mode

--- src/blocks/web/custom-script.tsx
+++ src/blocks/web/custom-script.tsx
@@ -1,26 +1,17 @@
-import * as React from "react";
 import { registerChaiBlock } from "@chaibuilder/runtime";
 import { Code } from "@chaibuilder/runtime/controls";
 import { ChaiBlock } from "../../core/types/ChaiBlock.ts";
 import { DiJavascript } from "react-icons/di";
-import EmptySlot from "../empty-slot.tsx";
 
-const CustomScript = React.memo(
-  (
-    props: ChaiBlock & {
-      script: string;
-    },
-  ) => {
-    const { script, inBuilder, blockProps } = props;
-    if (inBuilder)
-      return (
-        <div {...blockProps}>
-          <EmptySlot />
-        </div>
-      );
-    return <div dangerouslySetInnerHTML={{ __html: script }}></div>;
+const CustomScript = (
+  props: ChaiBlock & {
+    script: string;
   },
-);
+) => {
+  const { scripts, inBuilder, blockProps } = props;
+  if (inBuilder) return <div {...blockProps}></div>;
+  return <div dangerouslySetInnerHTML={{ __html: scripts }}></div>;
+};
 
 registerChaiBlock(CustomScript, {
   type: "CustomScript",
@@ -29,7 +20,7 @@ registerChaiBlock(CustomScript, {
   icon: DiJavascript,
   group: "advanced",
   props: {
-    script: Code({
+    scripts: Code({
       title: "Script",
       default: "",
       placeholder: "<script>console.log('Hello, world!');</script>",
