a7cceeb2149c164ecda1147484ff736dc5ed3e12
Rahul <rajamohanty12345@gmail.com>
2024-08-30 13:44:45 +0530

    fix: added opacity to cut blocks in outline panel

--- src/core/components/sidepanels/panels/outline/treeview/ListTree.tsx
+++ src/core/components/sidepanels/panels/outline/treeview/ListTree.tsx
@@ -7,6 +7,7 @@ import { cn } from "../../../../../functions/Functions.ts";
 import {
   useBlocksStore,
   useBuilderProp,
+  useCutBlockIds,
   useSelectedBlockIds,
   useSelectedStylingBlocks,
   useUpdateBlocksProps,
@@ -40,6 +41,8 @@ const Node = memo(({ node, style, dragHandle }: NodeRendererProps<any>) => {
   const outlineItems = useBuilderProp("outlineMenuItems", []);
 
   //const [, setHighlighted] = useHighlightBlockId();
+
+  const [cutBlocksIds] = useCutBlockIds();
 
   const [iframe] = useAtom<HTMLIFrameElement>(canvasIframeAtom);
 
@@ -170,6 +173,7 @@ const Node = memo(({ node, style, dragHandle }: NodeRendererProps<any>) => {
           isSelected ? "bg-blue-500 text-white" : "text-gray-600 hover:bg-gray-200 dark:hover:bg-gray-800",
           willReceiveDrop && canAcceptChildBlock(data._type, "Icon") ? "bg-green-200" : "",
           isDragging && "opacity-20",
+          cutBlocksIds.includes(id) && "pointer-events-none opacity-60",
         )}>
         <div className="flex items-center">
           <div
