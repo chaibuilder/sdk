f30fdd40f026a97134cddf1d80374e13494bd7a6
Suraj Air <surajair@gmail.com>
2024-09-13 10:11:29 +0530

    feat: added more components to export

--- src/core/main/index.ts
+++ src/core/main/index.ts
@@ -6,14 +6,25 @@ import Outline from "../components/sidepanels/panels/outline/treeview/ListTree.t
 import ImportHTML from "../components/sidepanels/panels/add-blocks/ImportHTML.tsx";
 import BlockPropsEditor from "../components/settings/BlockSettings.tsx";
 import BlockStyleEditor from "../components/settings/BlockStyling.tsx";
+import UILibrariesPanel from "../components/sidepanels/panels/add-blocks/UILibrariesPanel.tsx";
 import i18n from "../locales/load";
 
+export { AISetContext, AIUserPrompt } from "../components/AskAi.tsx";
 export { BlockAttributesEditor } from "../components/settings/new-panel/BlockAttributesEditor.tsx";
 export { ChaiBuilderEditor } from "./ChaiBuilderEditor";
 export { DarkMode as DarkModeSwitcher } from "../components/canvas/topbar/DarkMode.tsx";
 export { Breakpoints as ScreenSizes } from "../components/canvas/topbar/Breakpoints.tsx";
 export { UndoRedo } from "../components/canvas/topbar/UndoRedo.tsx";
-export { ChaiBuilderCanvas, ThemeOptions, Outline, ImportHTML, BlockPropsEditor, BlockStyleEditor, i18n };
+export {
+  ChaiBuilderCanvas,
+  ThemeOptions,
+  Outline,
+  ImportHTML,
+  BlockPropsEditor,
+  BlockStyleEditor,
+  i18n,
+  UILibrariesPanel as UILibraries,
+};
 export * from "../hooks";
 export { generateUUID as generateBlockId, cn as mergeClasses } from "../functions/Functions.ts";
 export { getBlocksFromHTML } from "../import-html/html-to-json.ts";
