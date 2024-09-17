1fb6cf6671f42b9e0386ca004697bc005731e2cd
Rahul <rajamohanty12345@gmail.com>
2024-09-17 15:46:55 +0530

    feat: added translation to theme and one of the web-block

--- src/web-blocks/button.tsx
+++ src/web-blocks/button.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ButtonIcon } from "@radix-ui/react-icons";
 import { Icon, Numeric, SelectOption, SingleLineText, Styles } from "@chaibuilder/runtime/controls";
 import { ChaiBlock } from "../core/types/ChaiBlock.ts";
+import { useTranslation } from "react-i18next";
 
 const Component = (
   block: ChaiBlock & {
@@ -35,26 +36,30 @@ const Component = (
   );
 };
 
-const Config = {
-  type: "Button",
-  label: "Button",
-  category: "core",
-  icon: ButtonIcon,
-  group: "basic",
-  props: {
-    content: SingleLineText({ title: "Label", default: "Button" }),
-    styles: Styles({ default: "text-white bg-primary px-4 py-2 rounded-global flex items-center" }),
-    icon: Icon({ title: "Icon", default: `` }),
-    iconSize: Numeric({ title: "Icon Size", default: 24 }),
-    iconPos: SelectOption({
-      title: "Icon Position",
-      default: "order-last",
-      options: [
-        { title: "Start", value: "order-first" },
-        { title: "End", value: "order-last" },
-      ],
-    }),
-  },
+const Config = () => {
+  const { t } = useTranslation();
+
+  return {
+    type: "Button",
+    label: t("Button"),
+    category: "core",
+    icon: ButtonIcon,
+    group: "basic",
+    props: {
+      content: SingleLineText({ title: t("Label"), default: t("Button") }),
+      styles: Styles({ default: "text-white bg-primary px-4 py-2 rounded-global flex items-center" }),
+      icon: Icon({ title: t("Icon"), default: `` }),
+      iconSize: Numeric({ title: t("Icon Size"), default: 24 }),
+      iconPos: SelectOption({
+        title: t("Icon Position"),
+        default: "order-last",
+        options: [
+          { title: t("Start"), value: "order-first" },
+          { title: t("End"), value: "order-last" },
+        ],
+      }),
+    },
+  };
 };
 
 export { Component, Config };
