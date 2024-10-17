84444536b81d3822db83d4d10ee1f8484340c9c6
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2024-10-17 14:35:58 +0530

    chore: added language prompt in ask ai callback promp arg

--- src/core/components/ChaiBuilderEditor.tsx
+++ src/core/components/ChaiBuilderEditor.tsx
@@ -3,14 +3,14 @@ import "../index.css";
 import i18n from "../locales/load.ts";
 import { FlagsProvider } from "flagged";
 import React, { useEffect, useMemo } from "react";
-import { each, noop, omit } from "lodash-es";
+import { each, noop, omit, isEmpty } from "lodash-es";
 import { FEATURE_TOGGLES } from "../../FEATURE_TOGGLES.tsx";
 import { chaiBuilderPropsAtom } from "../atoms/builder.ts";
 import { ErrorBoundary } from "react-error-boundary";
 import { RootLayout } from "./layout/RootLayout.tsx";
 import { builderStore } from "../atoms/store.ts";
 import { Toaster } from "../../ui";
-import { useBrandingOptions, useBuilderProp, useBuilderReset, useSavePage } from "../hooks";
+import { useBrandingOptions, useBuilderProp, useBuilderReset, useLanguages, useSavePage } from "../hooks";
 import { ChaiBuilderEditorProps } from "../types";
 import { dataProvidersAtom } from "../hooks/usePageDataProviders.ts";
 import { useBlocksStore } from "../history/useBlocksStoreUndoableActions.ts";
@@ -27,6 +27,7 @@ import { useExpandTree } from "../hooks/useExpandTree.ts";
 import { useIntervalEffect } from "@react-hookz/web";
 import { useWatchGlobalBlocks } from "../hooks/useGlobalBlocksStore.ts";
 import { useWatchRSCBlocks } from "../hooks/useWatchRSCBlocks.ts";
+import { ChaiBlock } from "../types/ChaiBlock.ts";
 const useAutoSave = () => {
   const { savePage } = useSavePage();
   const autoSaveSupported = useBuilderProp("autoSaveSupport", true);
@@ -41,6 +42,7 @@ const ChaiBuilderComponent = (props: ChaiBuilderEditorProps) => {
   const [, setAllBlocks] = useBlocksStore();
   const [, setBrandingOptions] = useBrandingOptions();
   const reset = useBuilderReset();
+  const { selectedLang } = useLanguages();
   const [saveState] = useAtom(builderSaveStateAtom);
   const RootLayoutComponent = useMemo(() => props.layout || RootLayout, [props.layout]);
   useAtom(selectedLibraryAtom);
@@ -64,7 +66,11 @@ const ChaiBuilderComponent = (props: ChaiBuilderEditorProps) => {
 
   useEffect(() => {
     // @ts-ignore
-    setAllBlocks(syncBlocksWithDefaults(props.blocks || []) as ChaiBlock[]);
+    if (isEmpty(selectedLang)) {
+      setAllBlocks(syncBlocksWithDefaults(props.blocks || []) as ChaiBlock[]);
+    } else {
+      setAllBlocks((props.blocks || []) as ChaiBlock[]);
+    }
     reset();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.blocks]);
