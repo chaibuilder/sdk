7efa7eb28fc83c78f3d4c87b5f8823e7aca58132
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2024-10-18 10:49:14 +0530

    refactor: removed db file and sqlite package, not in use

--- src/__dev/mock/db.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
-
-const sqlite = new Database("chaibuilder.db");
-const db = drizzle(sqlite);
-
-export { db };
