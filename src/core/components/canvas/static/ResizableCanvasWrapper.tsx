1528af031a01c1e4b1a157c3169b6f8c17d7bb3c
Suraj Air <surajair@gmail.com>
2024-11-04 13:41:21 +0530

    feat: deselect a block on clicking outside canvas

--- src/core/components/canvas/static/ResizableCanvasWrapper.tsx
+++ src/core/components/canvas/static/ResizableCanvasWrapper.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useRef } from "react";
 import { useDebouncedCallback, useResizeObserver } from "@react-hookz/web";
+import { useSelectedBlockIds, useSelectedStylingBlocks } from "../../../hooks";
 
 export const ResizableCanvasWrapper = ({ children, onMount, onResize }: any) => {
+  const [, setSelected] = useSelectedBlockIds();
+  const [, setSelectedStyles] = useSelectedStylingBlocks();
   const mainContentRef = useRef(null);
   const db = useDebouncedCallback(
     () => {
@@ -15,9 +18,16 @@ export const ResizableCanvasWrapper = ({ children, onMount, onResize }: any) => 
   useEffect(() => {
     const { clientWidth } = mainContentRef.current as HTMLDivElement;
     onMount(clientWidth);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const deselectSelected = () => {
+    setSelected([]);
+    setSelectedStyles([]);
+  };
+
   return (
-    <div id={"main-content"} className="h-full w-full p-8 pb-0" ref={mainContentRef}>
+    <div id={"main-content"} onClick={deselectSelected} className="h-full w-full p-8 pb-0" ref={mainContentRef}>
       {children}
     </div>
   );
