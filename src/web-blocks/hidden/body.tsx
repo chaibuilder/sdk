7597b0ee55f0ee5168d9b808ed0c0c8a76d98b2c
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2024-11-18 21:48:15 +0530

    chore: rjsf conversion of line break and body

--- src/web-blocks/hidden/body.tsx
+++ src/web-blocks/hidden/body.tsx
@@ -1,23 +1,21 @@
 import * as React from "react";
-import { Image, Styles } from "@chaibuilder/runtime/controls";
+import { ChaiBlockComponentProps, ChaiStyles, registerChaiBlockSchema, StylesProp } from "@chaibuilder/runtime";
 
-const BodyBlock = (
-  props: any & {
-    children: React.ReactNode;
-    styles: any;
-    tag: string;
-    backgroundImage: string;
-    blockProps: Record<string, string>;
-  },
-) => {
-  const { blockProps, backgroundImage, children, styles } = props;
+export type BodyProps = {
+  styles: ChaiStyles;
+  backgroundImage: string;
+  tag: string;
+};
+
+const BodyBlock = (props: ChaiBlockComponentProps<BodyProps>) => {
+  const { blockProps, backgroundImage, children, styles, tag } = props;
 
   let cssStyles = {};
   if (backgroundImage) {
     cssStyles = { backgroundImage: `url(${backgroundImage})` };
   }
 
-  return React.createElement("div", { ...blockProps, ...styles, style: cssStyles }, children);
+  return React.createElement(tag || "div", { ...blockProps, ...styles, style: cssStyles }, children);
 };
 
 const Config = {
@@ -26,10 +24,41 @@ const Config = {
   category: "core",
   group: "basic",
   hidden: true,
-  props: {
-    styles: Styles({ default: "font-body antialiased" }),
-    backgroundImage: Image({ title: "Background Image" }),
-  },
+  ...registerChaiBlockSchema({
+    properties: {
+      styles: StylesProp("font-body antialiased"),
+      backgroundImage: {
+        type: "string",
+        title: "Image",
+        default: "",
+        ui: { "ui:widget": "image" },
+      },
+      tag: {
+        type: "string",
+        default: "div",
+        title: "Tag",
+        oneOf: [
+          { const: "div", title: "div" },
+          { const: "header", title: "header" },
+          { const: "footer", title: "footer" },
+          { const: "section", title: "section" },
+          { const: "article", title: "article" },
+          { const: "aside", title: "aside" },
+          { const: "main", title: "main" },
+          { const: "nav", title: "nav" },
+          { const: "figure", title: "figure" },
+          { const: "details", title: "details" },
+          { const: "summary", title: "summary" },
+          { const: "dialog", title: "dialog" },
+          { const: "strike", title: "strike" },
+          { const: "caption", title: "caption" },
+          { const: "legend", title: "legend" },
+          { const: "figcaption", title: "figcaption" },
+          { const: "mark", title: "mark" },
+        ],
+      },
+    },
+  }),
   canAcceptBlock: () => true,
   canDelete: () => false,
   canMove: () => false,
