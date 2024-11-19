51750e6d52f0fe24f9c0a01b0d9f49726df0688f
Rahul <rajamohanty12345@gmail.com>
2024-11-19 14:15:51 +0530

    feat: added session storage for copy/paster

--- src/core/components/sidepanels/panels/outline/MultipleBlocksSelectionPopup.tsx
+++ src/core/components/sidepanels/panels/outline/MultipleBlocksSelectionPopup.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { CircleBackslashIcon, ClipboardIcon, ScissorsIcon, TrashIcon } from "@radix-ui/react-icons";
-import { useCopyBlockIds, useCutBlockIds, useRemoveBlocks, useSelectedBlockIds } from "../../../../hooks";
+import { useCopyBlockIds, useCutBlockIds, useRemoveBlocks, useSelectedBlockIds, useBlocksStore } from "../../../../hooks";
 import { Button, Card } from "../../../../../ui";
 
 export const MultipleBlocksSelectionPopup = () => {
@@ -8,12 +8,24 @@ export const MultipleBlocksSelectionPopup = () => {
   const [selectedIds, setSelectedIds] = useSelectedBlockIds();
   const [cutIds, setCutIds] = useCutBlockIds();
   const [copyIds, setCopyIds] = useCopyBlockIds();
+  const [blocks] = useBlocksStore();
   const removeBlocks = useRemoveBlocks();
   const { length } = selectedIds;
 
   if (length <= 1 || cutIds.length > 0 || copyIds.length > 0) {
     return null;
   }
+
+  const handleCopy = () => {
+    const selectedBlocks = selectedIds.map((id) => {
+      const block = blocks.find((b) => b._id === id);
+      return {
+        id,
+        data: block,
+      };
+    });
+    setCopyIds(selectedBlocks);
+  };
 
   return (
     <Card className="fixed bottom-10 left-1/2 z-50 mx-auto max-w-xl -translate-x-1/2 rounded-xl p-5 shadow-2xl">
@@ -30,7 +42,7 @@ export const MultipleBlocksSelectionPopup = () => {
           <Button
             variant="outline"
             size="sm"
-            onClick={() => setCopyIds(selectedIds)}
+            onClick={handleCopy}
             className="flex items-center gap-x-4 border">
             <ClipboardIcon /> {t("Copy")}
           </Button>
