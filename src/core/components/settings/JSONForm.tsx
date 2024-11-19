4ccf00310c54dd763d2e00b92c170c3e0b9aedc6
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2024-11-19 17:21:25 +0530

    chore: language suffix and accordian for list

--- src/core/components/settings/JSONForm.tsx
+++ src/core/components/settings/JSONForm.tsx
@@ -1,5 +1,5 @@
 import { memo, useEffect, useState } from "react";
-import { RJSFSchema, UiSchema } from "@rjsf/utils";
+import { FieldTemplateProps, RJSFSchema, UiSchema } from "@rjsf/utils";
 import RjForm from "@rjsf/core";
 import { BindingWidget } from "../../rjsf-widgets/binding.tsx";
 import { IconPickerField, ImagePickerField, RTEField } from "../../rjsf-widgets";
@@ -7,6 +7,11 @@ import validator from "@rjsf/validator-ajv8";
 import { useThrottledCallback } from "@react-hookz/web";
 import { CodeEditor } from "../../rjsf-widgets/Code.tsx";
 import { useLanguages } from "../../hooks/useLanguages.ts";
+import { useSelectedBlock } from "../../hooks/useSelectedBlockIds.ts";
+import { useRegisteredChaiBlocks } from "@chaibuilder/runtime";
+import { get, isEmpty } from "lodash";
+import { LANGUAGES } from "../../constants/LANGUAGES.ts";
+import { ChevronDown, ChevronRight, List, Plus } from "lucide-react";
 
 type JSONFormType = {
   id?: string;
@@ -15,6 +20,86 @@ type JSONFormType = {
   uiSchema: UiSchema;
   onChange: ({ formData }: any, key?: string) => void;
 };
+
+const CustomFieldTemplate = ({
+  id,
+  classNames,
+  label,
+  children,
+  errors,
+  help,
+  description,
+  hidden,
+  required,
+  schema,
+}: FieldTemplateProps) => {
+  const { selectedLang, fallbackLang, languages } = useLanguages();
+  const lang = isEmpty(languages) ? "" : isEmpty(selectedLang) ? fallbackLang : selectedLang;
+  const currentLanguage = get(LANGUAGES, lang, lang);
+
+  const selectedBlock = useSelectedBlock();
+  const registeredBlocks = useRegisteredChaiBlocks();
+  const i18nProps = get(registeredBlocks, [selectedBlock?._type, "i18nProps"], []) || [];
+  const [openedList, setOpenedList] = useState<null | string>(null);
+
+  if (hidden) {
+    return null;
+  }
+
+  const isCheckboxOrRadio = schema.type === "boolean";
+  if (isCheckboxOrRadio) return <div className={classNames}>{children}</div>;
+
+  const showLangSuffix = i18nProps?.includes(id.replace("root.", ""));
+
+  if (schema.type === "array") {
+    return (
+      <div className={`${classNames} relative`}>
+        {schema.title && (
+          <label
+            htmlFor={id}
+            onClick={() => setOpenedList(openedList ? null : id)}
+            className="flex cursor-pointer items-center gap-x-1 py-1 leading-tight duration-200 hover:bg-slate-100">
+            {openedList ? <ChevronDown className="h-3 w-3" /> : <ChevronRight className="h-3 w-3" />}
+            <List className="h-3 w-3" />
+            <span className="leading-tight">{label}</span>
+          </label>
+        )}
+        {openedList && (
+          <div className="p-2">
+            {description}
+            {children}
+            {errors}
+            {help}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div className={classNames}>
+      {schema.title && (
+        <label htmlFor={id} className={schema.type === "object" ? "pb-2" : ""}>
+          {label} {showLangSuffix && <small className="text-[9px] text-zinc-400"> {currentLanguage}</small>}
+          {required && schema.type !== "object" ? " *" : null}
+        </label>
+      )}
+      {description}
+      {children}
+      {errors}
+      {help}
+    </div>
+  );
+};
+
+const CustomAddButton = (props) => (
+  <button {...props} className="duration absolute right-2 top-2 cursor-pointer text-blue-400 hover:text-blue-500">
+    <div className="flex items-center gap-x-0.5 text-[11px] leading-tight">
+      <Plus className="h-3 w-3" /> <span>Add</span>
+    </div>
+  </button>
+);
+
 /**
  *
  * @param param0
@@ -22,8 +107,7 @@ type JSONFormType = {
  */
 export const JSONForm = memo(({ id, schema, uiSchema, formData, onChange }: JSONFormType) => {
   const [form, setForm] = useState<any>(formData);
-  const { selectedLang, fallbackLang, languages } = useLanguages();
-  const lang = languages.length === 0 ? "" : selectedLang.length ? selectedLang : fallbackLang;
+  const { selectedLang } = useLanguages();
 
   useEffect(() => {
     setForm(formData);
@@ -34,18 +118,25 @@ export const JSONForm = memo(({ id, schema, uiSchema, formData, onChange }: JSON
     async ({ formData }: any, id?: string) => {
       onChange({ formData }, id);
     },
-    [onChange],
+    [onChange, selectedLang],
     1000, // save only every 5 seconds
   );
 
   return (
     <RjForm
+      key={selectedLang}
       widgets={{
         binding: BindingWidget,
         richtext: RTEField,
         icon: IconPickerField,
         image: ImagePickerField,
         code: CodeEditor,
+      }}
+      templates={{
+        FieldTemplate: CustomFieldTemplate,
+        ButtonTemplates: {
+          AddButton: CustomAddButton,
+        },
       }}
       idSeparator="."
       autoComplete="off"
