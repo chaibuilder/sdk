c4b476f766f3cd227467c342315af5455af483c2
Suraj Air <surajair@gmail.com>
2024-11-20 12:48:22 +0530

    feat: handled default themeOptions

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,4 +1,8 @@
 export { convertToBlocks } from "./functions.ts";
 export { RenderChaiBlocks } from "./RenderChaiBlocks";
 export { getStylesForBlocks } from "./getTailwindCSS.ts";
-export { getThemeFontsLinkMarkup, getChaiThemeCssVariables } from "../core/components/canvas/static/ChaiThemeFn.ts";
+export {
+  getThemeFontsLinkMarkup,
+  getChaiThemeCssVariables,
+  getThemeFontsCSSImport,
+} from "../core/components/canvas/static/ChaiThemeFn.ts";
