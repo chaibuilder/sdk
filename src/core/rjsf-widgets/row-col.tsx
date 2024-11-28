867b2a5074e9ccf563d6d9b66ddc6835155b5097
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2024-11-28 10:33:25 +0530

    feat: new row and column block for grid layout

---
+++ src/core/rjsf-widgets/row-col.tsx
@@ -0,0 +1,36 @@
+import { useAddBlock, useBlocksStore, useSelectedBlock, useWrapperBlock } from "../hooks";
+import { filter } from "lodash-es";
+import { size } from "lodash";
+import { Badge } from "../../ui";
+import { Plus } from "lucide-react";
+
+const RowColField = () => {
+  const selectedBlock = useSelectedBlock();
+  const wrapperBlock = useWrapperBlock();
+  const [blocks] = useBlocksStore();
+  const { addCoreBlock } = useAddBlock();
+
+  if (!selectedBlock && !wrapperBlock) return null;
+
+  const rowBlock = selectedBlock?._type === "Row" ? selectedBlock : wrapperBlock;
+  const columnCounts = size(filter(blocks, { _parent: rowBlock?._id }));
+
+  return (
+    <div className="pt-1">
+      {columnCounts > 11 && (
+        <div className="text-[9px] leading-tight text-slate-400">Max limit reached. Only 12 columns allowed.</div>
+      )}
+      <div className="flex items-center gap-x-2 pt-1">
+        <Badge className="flex h-[22px] items-center justify-center rounded p-0 px-2 text-[14px]">{columnCounts}</Badge>
+        <button
+          disabled={columnCounts > 11}
+          className={`duratiom-300 flex items-center gap-x-1 rounded px-3 py-1 text-[11px] font-medium leading-tight ${columnCounts > 11 ? "cursor-not-allowed bg-gray-300 text-gray-500" : "border border-gray-400 bg-gray-100 hover:bg-slate-200"}`}
+          onClick={() => addCoreBlock({ type: "Column", styles: "#styles:," }, rowBlock?._id)}>
+          <Plus className="h-4 w-4" /> Add Column
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { RowColField };
---
+++ src/web-blocks/row-col.tsx
@@ -0,0 +1,117 @@
+import {
+  ChaiBlockComponentProps,
+  ChaiStyles,
+  registerChaiBlock,
+  registerChaiBlockSchema,
+  StylesProp,
+} from "@chaibuilder/runtime";
+import _, { get } from "lodash";
+import { Columns, Rows } from "lucide-react";
+
+export type RowProps = {
+  styles: ChaiStyles;
+};
+
+const Column = (props: ChaiBlockComponentProps<{ children: React.ReactNode; styles: ChaiStyles }>) => {
+  const { blockProps, children, styles, colSpan, tabletColSpan, desktopColSpan } = props;
+
+  const className = [
+    get(styles, "className", ""),
+    `col-span-${isNaN(colSpan) || !colSpan ? 6 : colSpan}`,
+    tabletColSpan ? `md:col-span-${tabletColSpan}` : "",
+    desktopColSpan ? `lg:col-span-${desktopColSpan}` : "",
+  ];
+  const _styles = { className: className.join(" ") };
+
+  return (
+    <div {...blockProps} {...styles} {..._styles}>
+      {children || (
+        <div className="min-h-12 h-full w-full border-2 border-dashed border-gray-400 bg-gray-100 dark:bg-gray-900" />
+      )}
+    </div>
+  );
+};
+
+registerChaiBlock(Column, {
+  type: "Column",
+  label: "Column",
+  hidden: true,
+  canMove: () => false,
+  canDelete: () => true,
+  icon: Columns,
+  wrapper: true,
+  canAcceptBlock: () => true,
+  ...registerChaiBlockSchema({
+    properties: {
+      styles: StylesProp(""),
+      colSpan: {
+        type: "number",
+        title: "Column Span",
+        default: 6,
+        enum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      },
+      tabletColSpan: {
+        type: "number",
+        title: "Column Span (Tablet)",
+        default: null,
+        enumNames: ["Default", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        enum: [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      },
+      desktopColSpan: {
+        type: "number",
+        title: "Column Span (Desktop)",
+        default: null,
+        enumNames: ["Default", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        enum: [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      },
+    },
+  }),
+});
+
+const Component = (props: ChaiBlockComponentProps<RowProps>) => {
+  const { blockProps, children, styles, gutter } = props;
+
+  const className = [get(styles, "className", ""), " grid grid-cols-12", isNaN(gutter) ? "" : `gap-[${gutter}px]`];
+  const _styles = { className: className.join(" ") };
+
+  return (
+    <div {...blockProps} {...styles} {..._styles}>
+      {children}
+    </div>
+  );
+};
+
+const Config = {
+  type: "Row",
+  label: "Row",
+  group: "basic",
+  icon: Rows,
+  blocks: () => [
+    { _type: "Row", _id: "row", styles: "#styles:,p-1" },
+    { _type: "Column", id: "column", _parent: "row", styles: "#styles:," },
+    { _type: "Column", id: "column", _parent: "row", styles: "#styles:," },
+  ],
+  category: "core",
+  wrapper: true,
+  canAcceptBlock: (childType) => childType === "Column",
+  ...registerChaiBlockSchema({
+    properties: {
+      styles: StylesProp(""),
+      colCount: {
+        type: "number",
+        title: "Columns Count",
+        default: 2,
+        minimum: 0,
+        ui: { "ui:widget": "colCount" },
+      },
+      gutter: {
+        type: "number",
+        title: "Gutter",
+        default: 16,
+        minimum: 0,
+      },
+    },
+  }),
+};
+
+export { Component, Config };
