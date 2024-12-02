7b8fc7872159a123ff8ad926bb27dc58f797dcb8
Rahul <rajamohanty12345@gmail.com>
2024-12-02 13:40:02 +0530

    feat: changed the UI of add block at bottom

--- src/core/components/canvas/static/AddBlockAtBottom.tsx
+++ src/core/components/canvas/static/AddBlockAtBottom.tsx
@@ -1,14 +1,21 @@
 import { useTranslation } from "react-i18next";
 import { CHAI_BUILDER_EVENTS } from "../../../events.ts";
 import { pubsub } from "../../../pubsub";
+import { PlusIcon } from "lucide-react";
 
 export const AddBlockAtBottom = () => {
   const { t } = useTranslation();
   return (
-    <button
-      onClick={() => pubsub.publish(CHAI_BUILDER_EVENTS.OPEN_ADD_BLOCK)}
-      className="mt-1 block w-full rounded-md bg-gray-100 p-2 text-black hover:bg-gray-200 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-700">
-      + {t("Add block")}
-    </button>
+    <div className="group relative w-full cursor-pointer">
+      <br />
+      <div
+        onClick={() => pubsub.publish(CHAI_BUILDER_EVENTS.OPEN_ADD_BLOCK)}
+        className="h-1 rounded bg-purple-500 opacity-0 duration-200 group-hover:opacity-100">
+        <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform items-center gap-x-1 rounded-full bg-purple-500 px-3 py-1 text-base leading-tight text-white hover:bg-purple-500">
+          <PlusIcon className="size-4 stroke-[3]" /> {t("Add block")}
+        </div>
+      </div>
+      <br />
+    </div>
   );
 };
