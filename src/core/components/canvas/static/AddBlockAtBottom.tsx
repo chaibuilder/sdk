6866c61746c7500d06341c7e9a32817dec3cbb71
Rahul <rajamohanty12345@gmail.com>
2024-12-04 11:01:54 +0530

    fix: fixed padding and text size of add block in canvas

--- src/core/components/canvas/static/AddBlockAtBottom.tsx
+++ src/core/components/canvas/static/AddBlockAtBottom.tsx
@@ -6,13 +6,13 @@ import { PlusIcon } from "lucide-react";
 export const AddBlockAtBottom = () => {
   const { t } = useTranslation();
   return (
-    <div className="group relative w-full cursor-pointer">
+    <div className="group relative w-full cursor-pointer py-2">
       <br />
       <div
         onClick={() => pubsub.publish(CHAI_BUILDER_EVENTS.OPEN_ADD_BLOCK)}
         className="h-1 rounded bg-purple-500 opacity-0 duration-200 group-hover:opacity-100">
-        <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform items-center gap-x-1 rounded-full bg-purple-500 px-3 py-1 text-sm leading-tight text-white hover:bg-purple-500">
-          <PlusIcon className="size-3 stroke-[3]" /> {t("Add block")}
+        <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform items-center gap-x-1 rounded-full bg-purple-500 px-3 py-1 text-xs leading-tight text-white hover:bg-purple-500">
+          <PlusIcon className="size-2.5 stroke-[3]" /> {t("Add block")}
         </div>
       </div>
       <br />
