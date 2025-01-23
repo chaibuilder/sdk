f18929184bfad2ccb06342f9ff3c3ebd8255aa9f
Suraj Air <surajair@gmail.com>
2025-01-23 15:02:22 +0530

    fix(globalblocks): use span instead of div to support sticky css property

--- src/web-blocks/global-block.tsx
+++ src/web-blocks/global-block.tsx
@@ -11,14 +11,14 @@ const Component = (props: ChaiBlockComponentProps<GlobalBlockProps>) => {
   if (inBuilder && !globalBlock) {
     return (
       <div
-        className="flex flex-col items-center justify-center gap-y-1 rounded-lg bg-gray-100 py-4 dark:bg-gray-800"
+        className="flex flex-col items-center justify-center py-4 bg-gray-100 rounded-lg gap-y-1 dark:bg-gray-800"
         {...blockProps}>
         <h1>Global Block - {globalBlock}</h1>
         <p>Choose a block from the sidebar to add it to this page.</p>
       </div>
     );
   }
-  return React.createElement("div", { ...blockProps }, children);
+  return React.createElement("span", { ...blockProps }, children);
 };
 
 const Config = {
