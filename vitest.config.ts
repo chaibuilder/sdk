d6feae71fe4d7345cd6b12f941bb6dcf15ee09fc
Suraj Air <surajair@gmail.com>
2025-01-24 13:06:40 +0530

    chore(jest-to-vitest): migrated unit testing from jest to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/vitest-setup.ts"],
+    exclude: [...configDefaults.exclude, "./e2e_tests"],
+  },
+});
