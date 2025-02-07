7b52fbc7d5d71a132fab31a4f634131990422af7
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2025-02-07 12:39:22 +0530

    feat: improve icon selection and rendering with enhanced UX

--- src/core/rjsf-widgets/Icon.tsx
+++ src/core/rjsf-widgets/Icon.tsx
@@ -1,67 +1,82 @@
 import { WidgetProps } from "@rjsf/utils";
-import { createRoot } from "react-dom/client";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import IconPicker, { IconPickerItem } from "react-icons-picker";
 
-const getSvgMarkup = (icon: string) => {
+const ICON_PICKER_CONTAINER_ID = "icon-picker-item-container";
+
+const removeSizeAttributes = (svgString: string): string => {
   try {
-    const container = document.getElementById("icon-picker-field") as HTMLElement;
-    const iconPickerContainer = document.createElement("div");
-    const root = createRoot(iconPickerContainer);
-    root.render(<IconPickerItem value={icon} />);
-    container.appendChild(iconPickerContainer);
-
-    iconPickerContainer.hidden = true;
-    const iconPickerMarkup = iconPickerContainer.innerHTML;
-    setTimeout(() => container.removeChild(iconPickerContainer), 100);
-
-    // // * Removing height and width from svg
-    const parser = new DOMParser();
-    const svgDoc = parser.parseFromString(iconPickerMarkup, "image/svg+xml");
-    // remove width and height
-    const svgElement = svgDoc.querySelector("svg");
-    if (svgElement) {
-      svgElement.removeAttribute("width");
-      svgElement.removeAttribute("height");
-    }
-    return new XMLSerializer().serializeToString(svgDoc);
-  } catch (err) {
-    console.error(err);
-    return "";
+    return svgString
+      .replace(/<svg([^>]*)\sheight="[^"]*"([^>]*)>/gi, "<svg$1$2>")
+      .replace(/<svg([^>]*)\swidth="[^"]*"([^>]*)>/gi, "<svg$1$2>");
+  } catch (error) {
+    return svgString;
   }
+};
+
+const getSvgMarkup = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        const iconPickerContainer = document.getElementById(ICON_PICKER_CONTAINER_ID);
+        let svgMarkup = iconPickerContainer?.innerHTML || "";
+        svgMarkup = removeSizeAttributes(svgMarkup);
+        resolve(svgMarkup);
+      } catch (err) {
+        reject(err);
+      }
+    }, 300);
+  });
 };
 
 const IconPickerField = ({ value, onChange }: WidgetProps) => {
   const { t } = useTranslation();
-  const handleIconChange = (icon: string) => {
-    onChange("<svg />");
-    const svgMarkup = getSvgMarkup(icon);
-    onChange(svgMarkup);
+  const [iconName, setIconName] = useState(null);
+
+  useEffect(() => {
+    setIconName(null);
+  }, [value]);
+
+  const handleIconChange = async (icon: string) => {
+    const lastIcon = iconName;
+    try {
+      setIconName(icon);
+      const svgMarkup = await getSvgMarkup();
+      onChange(svgMarkup);
+    } catch (error) {
+      setIconName(lastIcon);
+    }
   };
 
   return (
     <div className="mt-1 flex h-20 items-center gap-x-2" id="icon-picker-field">
-      <div className="group relative h-12 w-12">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: value ? value.replace("<svg", `<svg class="h-5 w-5"`) : "<svg class='h-5 w-5' />",
-          }}
-          className="absolute left-1/2 top-1/2 z-0 -translate-x-1/2 -translate-y-1/2 transform cursor-pointer bg-white"
-        />
+      <div className="relative h-12 w-12 cursor-pointer rounded-lg border duration-300 hover:bg-gray-100">
+        {iconName ? (
+          <div
+            id={ICON_PICKER_CONTAINER_ID}
+            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+            <IconPickerItem value={iconName} />
+          </div>
+        ) : value ? (
+          <div
+            className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2 transform"
+            dangerouslySetInnerHTML={{ __html: value }}
+          />
+        ) : (
+          <div
+            id="icon-picker-item-container"
+            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+            <IconPickerItem value={"BiSolidGrid"} />
+          </div>
+        )}
         <IconPicker
-          value={value ? value : "BiSolidGrid"}
+          value={iconName || value}
           onChange={handleIconChange}
           pickButtonStyle={{
-            position: "relative",
             height: "48px",
             width: "48px",
-            border: "1px solid #999",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: "6px",
-            backgroundColor: "transparent",
-            zIndex: 1,
+            opacity: 0,
           }}
           searchInputStyle={{
             backgroundColor: "transparent",
@@ -80,7 +95,7 @@ const IconPickerField = ({ value, onChange }: WidgetProps) => {
         onChange={(e) => onChange(e.target.value)}
         placeholder={t("Choose Icon or SVG")}
         className={
-          "no-scrollbar disabled:opacity-50; mt-1 flex w-full rounded-md border border-border bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed"
+          "no-scrollbar flex w-full rounded-md border border-border bg-background px-3 py-1.5 text-[10px] leading-4 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
         }
       />
     </div>
