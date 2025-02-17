755b258e777e631ae249414aaa8c29417a414bfe
Suraj Air <surajair@gmail.com>
2025-02-17 11:34:04 +0530

    chore: add live build configuration and npm script

---
+++ vite.config.live.ts
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "live",
+    emptyOutDir: true,
+  },
+});
