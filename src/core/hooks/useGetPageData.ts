0b233bad731c977de7142966fcfe747f9b08a572
Suraj Air <surajair@gmail.com>
2025-02-18 14:33:10 +0530

    chore: upgrade @chaibuilder/runtime to 2.0.0-beta.23 and enhance block data handling

--- src/core/hooks/useGetPageData.ts
+++ src/core/hooks/useGetPageData.ts
@@ -1,10 +1,26 @@
+import { getRegisteredChaiBlock } from "@chaibuilder/runtime";
+import { compact, get, map, memoize, omit } from "lodash-es";
 import { useCallback } from "react";
-import { map, omit } from "lodash-es";
-import { useBrandingOptions } from "./useBrandingOptions";
-import { useCurrentPage } from "./useCurrentPage";
 import { splitPageBlocks } from "../functions/split-blocks";
 import { useBlocksStore } from "../history/useBlocksStoreUndoableActions.ts";
 import { ChaiBlock } from "../types/ChaiBlock.ts";
+import { useBrandingOptions } from "./useBrandingOptions";
+import { useCurrentPage } from "./useCurrentPage";
+
+/**
+ * Get the builder props for a block
+ * @param type - The type of the block
+ * @returns The builder props for the block
+ */
+const getBlockBuilderProps = memoize((type: string) => {
+  const registeredBlock = getRegisteredChaiBlock(type);
+  const props = get(registeredBlock, "schema.properties", {});
+  return compact(
+    Object.keys(props).map((key) => {
+      return get(props[key], "builderProp", false) ? key : null;
+    }),
+  );
+});
 
 export const useGetPageData = () => {
   const [projectOptions] = useBrandingOptions();
@@ -12,9 +28,11 @@ export const useGetPageData = () => {
   const [presentBlocks] = useBlocksStore();
 
   return useCallback(() => {
-    const blocks = map(presentBlocks, (block: ChaiBlock) =>
-      omit(block, ["expanded", "order", "title", "siblings", "tempClasses"]),
-    );
+    // omit the builder props from the blocks as they are not needed for the page data
+    // and only used inside the builder
+    const blocks = map(presentBlocks, (block: ChaiBlock) => {
+      return omit(block, getBlockBuilderProps(block._type));
+    });
     const [pageFilteredBlocks = []] = splitPageBlocks(blocks);
     return {
       currentPage,
