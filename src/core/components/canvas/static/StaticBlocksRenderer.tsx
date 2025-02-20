cb6bd7c4b41f0e084947fbfd6efc650923bf94cb
Suraj Air <surajair@gmail.com>
2025-02-20 09:46:11 +0530

    refactor: remove binding feature toggle and simplify static blocks rendering

--- src/core/components/canvas/static/StaticBlocksRenderer.tsx
+++ src/core/components/canvas/static/StaticBlocksRenderer.tsx
@@ -1,18 +1,9 @@
-import { useFeature } from "flagged";
-import { filter, isEmpty } from "lodash-es";
+import { isEmpty } from "lodash-es";
 import { useBlocksStore } from "../../../history/useBlocksStoreUndoableActions.ts";
-import { ChaiBlock } from "../../../types/ChaiBlock.ts";
-import { BlocksRendererStatic } from "./BlocksRenderer";
 import { NewBlocksRenderer } from "./NewBlocksRenderer";
 
 export const StaticBlocksRenderer = () => {
   const [blocks] = useBlocksStore();
-  const binding = useFeature("binding");
-  const blocksHtml = isEmpty(blocks) ? null : binding ? (
-    <NewBlocksRenderer />
-  ) : (
-    <BlocksRendererStatic allBlocks={blocks} blocks={filter(blocks, (block: ChaiBlock) => isEmpty(block._parent))} />
-  );
-
+  const blocksHtml = isEmpty(blocks) ? null : <NewBlocksRenderer />;
   return <>{blocksHtml}</>;
 };
