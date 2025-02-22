393dee43bb0a19179527608c10228528628640b6
Suraj Air <surajair@gmail.com>
2025-02-22 22:03:19 +0530

    refactor: simplify useUpdateBlockAtom with direct atom manipulation

--- src/core/hooks/useUpdateBlockAtom.ts
+++ src/core/hooks/useUpdateBlockAtom.ts
@@ -1,38 +1,35 @@
+import { atom, useSetAtom } from "jotai";
 import { useAtomCallback } from "jotai/utils";
 import { find } from "lodash-es";
 import { useCallback } from "react";
 import { pageBlocksAtom } from "../atoms/blocks";
 import { ChaiBlock } from "../types/ChaiBlock.ts";
-import { useBlocksStore } from "./hooks.ts";
+
+const writeAtomValue = atom(
+  null, // it's a convention to pass `null` for the first argument
+  (get, set, { id, props }: { id: string; props: Record<string, any> }) => {
+    const blockAsAtoms = get(pageBlocksAtom);
+    console.log("From Atom callback", blockAsAtoms);
+    const blockAtom = find(blockAsAtoms, (b) => b._id === id);
+    if (!blockAtom) {
+      throw new Error(`Block with id ${id} not found`);
+    }
+    return set(blockAtom._atom, { ...(get(blockAtom._atom) as any), ...props });
+  },
+);
 
 export const useUpdateBlockAtom = () => {
-  const [blocks] = useBlocksStore();
-  console.log("Default Blocks: ", blocks);
-  return useAtomCallback(
-    useCallback(
-      (get, set, { id, props }: { id: string; props: Record<string, any> }) => {
-        const blockAsAtoms = get(pageBlocksAtom);
-        console.log("From Atom callback", blocks);
-        const blockAtom = find(blockAsAtoms, (b) => b._id === id);
-        if (!blockAtom) {
-          console.warn(`Block with id ${id} not found`);
-          return;
-        }
-        return set(blockAtom._atom, { ...(get(blockAtom._atom) as any), ...props });
-      },
-      [pageBlocksAtom],
-    ),
-  );
+  return useSetAtom(writeAtomValue);
 };
 
 export const useGetBlockAtomValue = () => {
   return useAtomCallback(
     useCallback((get, _set, id: string) => {
       const blockAsAtoms = get(pageBlocksAtom);
+      console.log("Block As Atoms", blockAsAtoms);
       const blockAtom = find(blockAsAtoms, (b) => b._id === id);
       if (!blockAtom) {
-        console.warn(`Block with id ${id} not found`);
-        return;
+        throw new Error(`Block with id ${id} not found`);
       }
       return get(blockAtom._atom) as ChaiBlock;
     }, []),
