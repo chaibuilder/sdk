c83824904f1ffe9ec44156bcbb7603dc0ae40fae
Suraj Air <surajair@gmail.com>
2025-02-22 14:33:54 +0530

    refactor: enhance useUpdateBlockAtom with block store logging and dependency update

--- src/core/hooks/useUpdateBlockAtom.ts
+++ src/core/hooks/useUpdateBlockAtom.ts
@@ -3,17 +3,25 @@ import { find } from "lodash-es";
 import { useCallback } from "react";
 import { pageBlocksAtom } from "../atoms/blocks";
 import { ChaiBlock } from "../types/ChaiBlock.ts";
+import { useBlocksStore } from "./hooks.ts";
+
 export const useUpdateBlockAtom = () => {
+  const [blocks] = useBlocksStore();
+  console.log("Default Blocks: ", blocks);
   return useAtomCallback(
-    useCallback((get, set, { id, props }: { id: string; props: Record<string, any> }) => {
-      const blockAsAtoms = get(pageBlocksAtom);
-      const blockAtom = find(blockAsAtoms, (b) => b._id === id);
-      if (!blockAtom) {
-        console.warn(`Block with id ${id} not found`);
-        return;
-      }
-      return set(blockAtom._atom, { ...(get(blockAtom._atom) as any), ...props });
-    }, []),
+    useCallback(
+      (get, set, { id, props }: { id: string; props: Record<string, any> }) => {
+        const blockAsAtoms = get(pageBlocksAtom);
+        console.log("From Atom callback", blocks);
+        const blockAtom = find(blockAsAtoms, (b) => b._id === id);
+        if (!blockAtom) {
+          console.warn(`Block with id ${id} not found`);
+          return;
+        }
+        return set(blockAtom._atom, { ...(get(blockAtom._atom) as any), ...props });
+      },
+      [pageBlocksAtom],
+    ),
   );
 };
 
