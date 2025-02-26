530f4c6824d4340047fe76bddd458e6fb9c80acb
Suraj Air <surajair@gmail.com>
2025-02-26 12:01:48 +0530

    refactor: improve breakpoint selector and canvas topbar UI

--- src/core/components/settings/new-panel/BreakpointSelector.tsx
+++ src/core/components/settings/new-panel/BreakpointSelector.tsx
@@ -1,44 +1,24 @@
-import { includes, map, toUpper } from "lodash-es";
-import { Button } from "../../../../ui";
+import { useMemo } from "react";
+import { useCanvasWidth } from "../../../hooks";
+import { Breakpoints, WEB_BREAKPOINTS } from "../../canvas/topbar/Breakpoints";
 
-import { useBuilderProp, useCanvasWidth, useSelectedBreakpoints } from "../../../hooks";
-import { BreakpointCardProps, BreakpointItemType, WEB_BREAKPOINTS } from "../../canvas/topbar/Breakpoints";
-
-const BreakpointCard = ({ currentBreakpoint, breakpoint, width, icon, onClick }: BreakpointCardProps) => {
-  return (
-    <Button
-      onClick={() => onClick(width)}
-      size="sm"
-      className="h-6 w-6 rounded-md p-1"
-      variant={breakpoint === currentBreakpoint ? "default" : "secondary"}>
-      {icon}
-    </Button>
-  );
-};
 export function BreakpointSelector() {
-  const [, breakpoint, setNewWidth] = useCanvasWidth();
-  const [selectedBreakpoints] = useSelectedBreakpoints();
-  const breakpoints = useBuilderProp("breakpoints", WEB_BREAKPOINTS);
+  const [, breakpoint] = useCanvasWidth();
 
-  if (breakpoints.length < 4) {
-    return (
-      <div className="flex items-center rounded-md">
-        {map(breakpoints, (bp) => (
-          <BreakpointCard {...bp} onClick={setNewWidth} key={bp.breakpoint} currentBreakpoint={breakpoint} />
-        ))}
-      </div>
-    );
-  }
+  const message = useMemo(() => {
+    const currentBreakpoint = WEB_BREAKPOINTS.find((bp) => bp.breakpoint === breakpoint);
+    return currentBreakpoint?.content;
+  }, [breakpoint, WEB_BREAKPOINTS]);
 
   return (
-    <div className="flex items-center justify-start rounded-md bg-muted px-2 py-1">
-      <p className="text-xs text-muted-foreground">Breakpoints: </p>
-      {map(
-        breakpoints.filter((bp: BreakpointItemType) => includes(selectedBreakpoints, toUpper(bp.breakpoint))),
-        (bp: BreakpointItemType) => (
-          <BreakpointCard {...bp} onClick={setNewWidth} key={bp.breakpoint} currentBreakpoint={breakpoint} />
-        ),
-      )}
-    </div>
+    <>
+      <div className="sticky top-0 z-10 flex items-center justify-start bg-muted px-2 py-1 shadow-sm">
+        <p className="text-xs text-muted-foreground">Screen:&nbsp;</p>
+        <Breakpoints showTooltip={false} />
+      </div>
+      <div className="flex items-center justify-between rounded-md rounded-t-none border border-border p-1">
+        <p className="flex-1 text-[10px] text-muted-foreground">{message}</p>
+      </div>
+    </>
   );
 }
