d66a9b8d2419032a131165bc5e47083e9c7db43c
Ali Ghaleyan <46399482+AliGhaleyan@users.noreply.github.com>
2025-03-14 13:02:28 +0330

    feat: separate and export the JSONForm custom field template (#232)

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -17,6 +17,7 @@ export { DefaultChaiBlocks } from "../components/sidepanels/panels/add-blocks/De
 export { DarkMode as DarkModeSwitcher } from "../components/canvas/topbar/DarkMode.tsx";
 export { Breakpoints as ScreenSizes } from "../components/canvas/topbar/Breakpoints.tsx";
 export { UndoRedo } from "../components/canvas/topbar/UndoRedo.tsx";
+export { default as JSONFormFieldTemplate } from "./settings/JSONFormFieldTemplate.tsx";
 export {
   ChaiBuilderCanvas,
   ThemeOptions,
