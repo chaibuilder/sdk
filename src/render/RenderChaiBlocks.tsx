4aabf2ff61c73c869f47642fd775f2f132bf385e
Suraj Air <surajair@gmail.com>
2025-03-17 11:17:06 +0530

    feat: conditionally render MicrosoftClarity component and add unique keys to RenderChaiBlocks

--- src/render/RenderChaiBlocks.tsx
+++ src/render/RenderChaiBlocks.tsx
@@ -185,6 +185,7 @@ export function RenderChaiBlocks({
           ...runtimeProps,
           metadata,
           lang: lang || fallbackLang,
+          key: block._id,
         },
         ["_parent"],
       );
@@ -195,6 +196,7 @@ export function RenderChaiBlocks({
           <Suspense fallback={createElement(suspenseFallback)} key={block._id}>
             {/* @ts-ignore */}
             <AsyncPropsBlock
+              key={block._id}
               dataProviderMetadataCallback={dataProviderMetadataCallback}
               lang={lang || fallbackLang}
               metadata={metadata}
