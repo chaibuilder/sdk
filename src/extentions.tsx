2b61ace7d1940b02359ccee0206d6590a8bf9c10
Suraj Air <surajair@gmail.com>
2025-04-04 14:11:17 +0530

    feat: add new sidebar panels for drawer, modal, and overlay views in extensions

--- src/extentions.tsx
+++ src/extentions.tsx
@@ -1,5 +1,6 @@
 import { ChaiFontViaSrc, ChaiFontViaUrl, registerChaiFont } from "@chaibuilder/runtime";
-import { ThermometerIcon } from "lucide-react";
+import { IconJarLogoIcon } from "@radix-ui/react-icons";
+import { BotIcon, BusIcon, ThermometerIcon } from "lucide-react";
 import registerCustomBlocks from "./__dev/blocks";
 import { registerChaiSidebarPanel } from "./core/main";
 
@@ -19,5 +20,32 @@ registerChaiSidebarPanel("panel-1", {
   icon: <ThermometerIcon />,
   label: "Panel 1",
   position: "top",
+  showIf: () => true,
+});
+
+registerChaiSidebarPanel("drawer-panel", {
+  component: () => "Drawer Panel",
+  icon: <IconJarLogoIcon className="h-5 w-5" />,
+  label: "Drawer Panel",
+  position: "top",
+  view: "drawer",
+  showIf: () => true,
+});
+
+registerChaiSidebarPanel("modal-panel", {
+  component: () => "Modal Panel",
+  icon: <BotIcon className="h-5 w-5" />,
+  label: "Modal Panel",
+  position: "top",
+  view: "modal",
+  showIf: () => true,
+});
+
+registerChaiSidebarPanel("overlay-panel", {
+  component: () => "Overlay Panel",
+  icon: <BusIcon className="h-5 w-5" />,
+  label: "Overlay Panel",
+  position: "top",
+  view: "overlay",
   showIf: () => true,
 });
