ffb1a047566e1f57659696cec54a0e94947f5fca
Suraj Air <surajair@gmail.com>
2025-04-04 14:00:34 +0530

    refactor: streamline Editor and RootLayout components
     removing unused imports and updating sidebar panel management

---
+++ src/core/extensions/sidebar-panels.tsx
@@ -0,0 +1,30 @@
+import { filter, has, set, values } from "lodash-es";
+import { ComponentType, useMemo } from "react";
+
+interface ChaiSidebarPanel<T> {
+  id: string;
+  position: "top" | "bottom";
+  view?: "standard" | "modal" | "overlay" | "drawer";
+  icon: React.ReactNode;
+  label: string;
+  component: ComponentType;
+  showIf?: (params: T) => boolean;
+  width?: number;
+  isInternal?: boolean;
+}
+
+const CHAI_BUILDER_PANELS: Record<string, ChaiSidebarPanel<any>> = {};
+
+export const registerChaiSidebarPanel = <T extends Record<string, any>>(
+  panelId: string,
+  panelOptions: Omit<ChaiSidebarPanel<T>, "id">,
+) => {
+  if (has(CHAI_BUILDER_PANELS, panelId)) {
+    console.warn(`Panel ${panelId} already registered. Overriding...`);
+  }
+  set(CHAI_BUILDER_PANELS, panelId, { id: panelId, ...panelOptions });
+};
+
+export const useChaiSidebarPanels = (position: "top" | "bottom") => {
+  return useMemo(() => filter(values(CHAI_BUILDER_PANELS), (panel) => panel.position === position), [position]);
+};
