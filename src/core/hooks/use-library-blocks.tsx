1650f44a6d9c62d5b189a8dcb8872c934d503d12
Suraj Air <surajair@gmail.com>
2025-04-14 07:15:38 +0530

    refactor: enhance error handling and resetLibrary functionality in useLibraryBlocks hook

--- src/core/hooks/use-library-blocks.tsx
+++ src/core/hooks/use-library-blocks.tsx
@@ -13,20 +13,29 @@ export const useLibraryBlocks = (library?: Partial<ChaiUILibrary> & { id: string
   const blocks = get(libraryBlocks, `${library?.id}.blocks`, null);
   const state = get(libraryBlocks, `${library?.id}.loading`, "idle");
   const loadingRef = useRef("idle");
+
   useEffect(() => {
     (async () => {
       if (state === "complete" || loadingRef.current === "loading") return;
       loadingRef.current = "loading";
       setLibraryBlocks((prev) => ({ ...prev, [library?.id]: { loading: "loading", blocks: [] } }));
-      const libraryBlocks: ChaiUILibraryBlock[] = await getBlocks(library);
-      loadingRef.current = "idle";
-      setLibraryBlocks((prev) => ({ ...prev, [library?.id]: { loading: "complete", blocks: libraryBlocks || [] } }));
+      try {
+        const libraryBlocks: ChaiUILibraryBlock[] = await getBlocks(library);
+        loadingRef.current = "idle";
+        setLibraryBlocks((prev) => ({ ...prev, [library?.id]: { loading: "complete", blocks: libraryBlocks || [] } }));
+      } catch (error) {
+        loadingRef.current = "idle";
+        setLibraryBlocks((prev) => ({ ...prev, [library?.id]: { loading: "complete", blocks: [] } }));
+      }
     })();
   }, [library, blocks, state, loadingRef, setLibraryBlocks, getBlocks]);
 
-  const resetLibrary = useCallback(() => {
-    setLibraryBlocks((prev) => ({ ...prev, [library?.id]: { loading: "idle", blocks: [] } }));
-  }, [library, setLibraryBlocks]);
+  const resetLibrary = useCallback(
+    (libraryId: string) => {
+      setLibraryBlocks((prev) => ({ ...prev, [libraryId]: { loading: "idle", blocks: [] } }));
+    },
+    [setLibraryBlocks],
+  );
 
   return { data: blocks || [], isLoading: state === "loading", resetLibrary };
 };
