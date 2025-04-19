d6f1b042b92082366d932434ffa9549da2ed211d
Suraj Air <surajair@gmail.com>
2025-04-19 18:45:40 +0530

    fix: build issue on live site due to incorrect vite config

--- vite.config.live.ts
+++ vite.config.live.ts
@@ -1,11 +1,16 @@
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "live",
     emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
