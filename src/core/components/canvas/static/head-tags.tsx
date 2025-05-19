be39684eba283ef906b2e415af620f1a809e4a16
Suraj Air <surajair@gmail.com>
2025-05-19 12:27:04 +0530

    refactor: simplify block selection highlighting by extracting into separate components

--- src/core/components/canvas/static/head-tags.tsx
+++ src/core/components/canvas/static/head-tags.tsx
@@ -1,4 +1,3 @@
-import { draggedBlockAtom, dropTargetBlockIdAtom } from "@/core/components/canvas/dnd/atoms";
 import {
   getChaiThemeOptions,
   getThemeCustomFontFace,
@@ -14,41 +13,21 @@ import aspectRatio from "@tailwindcss/aspect-ratio";
 import containerQueries from "@tailwindcss/container-queries";
 import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
-import { useAtom } from "jotai";
 import { filter, get, has, map } from "lodash-es";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import plugin from "tailwindcss/plugin";
 
 export const HeadTags = () => {
   console.log("HeadTags");
   const [chaiTheme] = useTheme();
   const chaiThemeOptions = useThemeOptions();
-  const [selectedBlockIds] = useSelectedBlockIds();
   const [darkMode] = useDarkMode();
-
-  const [stylingBlockIds] = useSelectedStylingBlocks();
-  const [draggedBlock] = useAtom(draggedBlockAtom);
-  const [dropTargetId] = useAtom(dropTargetBlockIdAtom);
-
   const { document: iframeDoc, window: iframeWin } = useFrame();
-
-  const [highlightedBlockStyle] = useState(iframeDoc?.getElementById("highlighted-block") as HTMLStyleElement);
-  const [selectedBlockStyle] = useState<HTMLStyleElement>(
-    iframeDoc?.getElementById("selected-block") as HTMLStyleElement,
-  );
-  const [selectedStylingBlocks] = useState<HTMLStyleElement>(
-    iframeDoc?.getElementById("selected-styling-block") as HTMLStyleElement,
-  );
-  const [draggedBlockStyle] = useState<HTMLStyleElement>(
-    iframeDoc?.getElementById("dragged-block") as HTMLStyleElement,
-  );
 
   useEffect(() => {
     if (darkMode) iframeDoc?.documentElement.classList.add("dark");
     else iframeDoc?.documentElement.classList.remove("dark");
   }, [darkMode, iframeDoc]);
-
-  const registeredFonts = useRegisteredFonts();
 
   useEffect(() => {
     // @ts-ignore
@@ -90,47 +69,48 @@ export const HeadTags = () => {
     };
   }, [chaiTheme, chaiThemeOptions, iframeWin]);
 
-  useEffect(() => {
-    if (!selectedBlockStyle) return;
-    selectedBlockStyle.textContent = `${map(selectedBlockIds, (id) => `[data-block-id="${id}"]`).join(",")}{
-                outline: 1px solid ${
-                  selectedBlockIds.length === 1 ? "#42a1fc !important" : "orange !important"
-                }; outline-offset: -1px;
-            }`;
-  }, [selectedBlockIds, selectedBlockStyle]);
-
-  useEffect(() => {
-    draggedBlockStyle.textContent = draggedBlock
-      ? `[data-block-id="${draggedBlock._id}"], [data-block-id="${draggedBlock._id}"] > * { pointer-events: none !important; opacity: 0.6 !important}`
-      : "";
-  }, [draggedBlock, draggedBlockStyle]);
-
-  useEffect(() => {
-    if (!highlightedBlockStyle) return;
-    highlightedBlockStyle.textContent = `[data-highlighted="true"]{ outline: 1px solid #42a1fc !important; outline-offset: -1px;}`;
-  }, [highlightedBlockStyle]);
-
-  useEffect(() => {
-    if (!selectedStylingBlocks) return;
-    selectedStylingBlocks.textContent = `${map(stylingBlockIds, ({ id }: any) => `[data-style-id="${id}"]`).join(",")}{
-                outline: 1px solid orange !important; outline-offset: -1px;
-            }`;
-  }, [stylingBlockIds, selectedStylingBlocks]);
-
-  useEffect(() => {
-    iframeDoc.querySelector(`#drop-target-block`).innerHTML = dropTargetId
-      ? `[data-block-id="${dropTargetId}"]{ outline: 1px dashed orange !important; outline-offset: -1px;}`
-      : "";
-  }, [dropTargetId, iframeDoc]);
   return (
     <>
       <CssThemeVariables theme={chaiTheme as ChaiBuilderThemeValues} />
       <Fonts />
+      <SelectedBlocks />
+      <SelectedStylingBlocks />
+      <BlockSelectionHighlighter />
     </>
   );
 };
 
-export const Fonts = () => {
+const BlockSelectionHighlighter = () => {
+  const [selectedBlockIds] = useSelectedBlockIds();
+  const styles = useMemo(() => {
+    return `${map(selectedBlockIds, (id) => `[data-block-id="${id}"]`).join(",")}{
+                outline: 1px solid orange !important; outline-offset: -1px;
+            }`;
+  }, [selectedBlockIds]);
+  return <style id="highlighted-block" dangerouslySetInnerHTML={{ __html: styles }} />;
+};
+
+const SelectedStylingBlocks = () => {
+  const [selectedStylingBlocks] = useSelectedStylingBlocks();
+  const styles = useMemo(() => {
+    return `${map(selectedStylingBlocks, ({ id }: any) => `[data-style-id="${id}"]`).join(",")}{
+                outline: 1px solid orange !important; outline-offset: -1px;
+            }`;
+  }, [selectedStylingBlocks]);
+  return <style id="selected-styling-blocks" dangerouslySetInnerHTML={{ __html: styles }} />;
+};
+
+const SelectedBlocks = () => {
+  const [selectedBlockIds] = useSelectedBlockIds();
+  const styles = useMemo(() => {
+    return `${map(selectedBlockIds, (id) => `[data-block-id="${id}"]`).join(",")}{
+                outline: 1px solid #42a1fc !important; outline-offset: -1px;
+            }`;
+  }, [selectedBlockIds]);
+  return <style id="selected-blocks" dangerouslySetInnerHTML={{ __html: styles }} />;
+};
+
+const Fonts = () => {
   const [chaiTheme] = useTheme();
   const registeredFonts = useRegisteredFonts();
   const pickedFonts = useMemo(() => {
