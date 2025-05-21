c120130adb74ce5959eb29afaaa87ed395084a3d
Suraj Air <surajair@gmail.com>
2025-05-21 10:52:25 +0530

    fix: handle empty repeater items array and cleanup async props logic

--- src/core/async-props/use-async-props.ts
+++ src/core/async-props/use-async-props.ts
@@ -1,7 +1,7 @@
 import { ChaiBlock } from "@chaibuilder/runtime";
 import { isObject } from "@rjsf/utils";
 import { atom, useAtom, useSetAtom } from "jotai";
-import { get, isArray, isEmpty, isFunction, pick, startsWith, values } from "lodash-es";
+import { get, isArray, isFunction, isUndefined, pick, startsWith, values } from "lodash-es";
 import { useEffect, useState } from "react";
 import { COLLECTION_PREFIX } from "../constants/STRINGS";
 import { useBuilderProp } from "../hooks";
@@ -33,10 +33,9 @@ export const useAsyncProps = (
 
   const getAsyncBlockProps = useBuilderProp("getBlockAsyncProps", async (_args: { block: ChaiBlock }) => ({}));
   const setBlockRepeaterDataAtom = useSetAtom(blockRepeaterDataAtom);
-  const depsString = JSON.stringify([block?._id, ...values(pick(block, dependencies))]);
-  const status = get(asyncProps, `status`);
+  const depsString = JSON.stringify([block?._id, ...values(pick(block, dependencies ?? []))]);
   const isCollectionRepeater = block?._type === "Repeater" && startsWith(block.repeaterItems, `{{${COLLECTION_PREFIX}`);
-  const isCustomBlockDataProvider = block?._type !== "Repeater" && !isEmpty(dependencies);
+  const isCustomBlockDataProvider = block?._type !== "Repeater" && !isUndefined(dependencies);
 
   useEffect(() => {
     if (!block) return;
@@ -55,12 +54,11 @@ export const useAsyncProps = (
     setAsyncProps((prev) => ({ ...prev, status: "loading", props: {} }));
     getAsyncBlockProps({ block })
       .then((props = {}) => {
-        console.log("Returned Props", isCollectionRepeater, props);
         if (isCollectionRepeater) {
           // set the props to a global state
           setBlockRepeaterDataAtom((prev) => ({
             ...prev,
-            [block._id]: { status: "loaded", props: isArray(props) ? props : {}, repeaterItems: block.repeaterItems },
+            [block._id]: { status: "loaded", props: isArray(props) ? props : [], repeaterItems: block.repeaterItems },
           }));
           setAsyncProps((prev) => ({ ...prev, status: "loaded" }));
         } else {
@@ -71,7 +69,7 @@ export const useAsyncProps = (
         if (isCollectionRepeater) {
           setBlockRepeaterDataAtom((prev) => ({
             ...prev,
-            [block._id]: { status: "error", error, props: {} },
+            [block._id]: { status: "error", error, props: [] },
           }));
           setAsyncProps((prev) => ({ ...prev, status: "error", error }));
         } else {
@@ -80,6 +78,7 @@ export const useAsyncProps = (
       });
   }, [block?._id, depsString, isCollectionRepeater, isCustomBlockDataProvider, mockDataProvider]);
 
+  const status = get(asyncProps, `status`);
   return {
     $loading: status === "loading",
     ...(block ? get(asyncProps, `props`, {}) : {}),
