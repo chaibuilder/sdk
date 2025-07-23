7ebfe7957030df5f02481702e915bc8f80df70ae
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-07-23 14:56:42 +0530

    feat: update class tag UI with Tailwind icon and improved hover states

--- src/core/components/settings/new-panel/manual-classes.tsx
+++ src/core/components/settings/new-panel/manual-classes.tsx
@@ -215,12 +215,27 @@ export function ManualClasses() {
                     }
                   }, 10);
                 }}
-                className="flex cursor-default items-center gap-x-1 truncate break-words rounded border border-border bg-gray-200 p-px  px-1 text-[11px] text-gray-600 hover:border-gray-300 hover:pl-4 group-hover:pl-4 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300">
+                className="flex cursor-default items-center gap-x-1 truncate break-words border border-border bg-gray-200 p-px pl-5 pr-2 text-[11px] text-gray-600 hover:border-gray-300 hover:pl-5 group-hover:pl-5 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300">
                 {cls}
                 <Cross2Icon
                   onClick={() => removeClassesFromBlocks(selectedIds, [cls], true)}
-                  className="absolute left-1 mr-0.5 hidden h-3 w-3 rounded-full bg-red-400 text-white hover:bg-red-500 hover:text-white group-hover:block"
+                  className="absolute left-0.5 mr-0.5 hidden group-hover:block h-4 w-4 bg-gray-500 font-bold group-hover:bg-gray-300 group-hover:text-red-500 "
                 />
+                <svg
+                  className="absolute left-0.5 mr-0.5 group-hover:hidden h-4 w-4 "
+                  fill="#9ca19d"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlSpace="preserve">
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M12 6.036c-2.667 0-4.333 1.325-5 3.976 1-1.325 2.167-1.822 3.5-1.491.761.189 1.305.738 1.906 1.345C13.387 10.855 14.522 12 17 12c2.667 0 4.333-1.325 5-3.976-1 1.325-2.166 1.822-3.5 1.491-.761-.189-1.305-.738-1.907-1.345-.98-.99-2.114-2.134-4.593-2.134zM7 12c-2.667 0-4.333 1.325-5 3.976 1-1.326 2.167-1.822 3.5-1.491.761.189 1.305.738 1.907 1.345.98.989 2.115 2.134 4.594 2.134 2.667 0 4.333-1.325 5-3.976-1 1.325-2.167 1.822-3.5 1.491-.761-.189-1.305-.738-1.906-1.345C10.613 13.145 9.478 12 7 12z"></path>
+                  </g>
+                </svg>
               </button>
             </div>
           ),
