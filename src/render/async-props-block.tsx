207be9a98cd41422f6c4f8a1ac9dd21fb0a2fc21
Suraj Air <surajair@gmail.com>
2025-08-05 20:07:06 +0530

    refactor: remove data provider caching to fix stale data issues

--- src/render/async-props-block.tsx
+++ src/render/async-props-block.tsx
@@ -1,5 +1,5 @@
 import { ChaiBlock, ChaiPageProps } from "@chaibuilder/runtime";
-import { has, isFunction, omit, pick } from "lodash-es";
+import { has, isFunction, omit } from "lodash-es";
 import React from "react";
 
 type DataProvider = (props: {
@@ -9,17 +9,6 @@ type DataProvider = (props: {
   block: ChaiBlock;
   pageProps: ChaiPageProps;
 }) => Record<string, any> | Promise<Record<string, any>>;
-
-// Cache for memoizing data provider results
-const dataProviderCache = new Map<string, Promise<Record<string, any>>>();
-
-// Create a cache key based on the data provider arguments
-function createCacheKey(args: { block: ChaiBlock; lang: string }): string {
-  return JSON.stringify({
-    blockType: omit(args.block, "_id", "_name", "_parent"),
-    lang: args.lang,
-  });
-}
 
 export default async function DataProviderPropsBlock(props: {
   lang: string;
@@ -37,22 +26,8 @@ export default async function DataProviderPropsBlock(props: {
     draft: props.draft,
     inBuilder: false,
   };
-  const cacheKey = createCacheKey(pick(dataProviderArgs, ["block", "lang"]));
-  // Check if we have a cached result
-  let dataPropsPromise = dataProviderCache.get(cacheKey);
 
-  if (!dataPropsPromise) {
-    // Create and cache the promise, ensuring it's always a promise
-    dataPropsPromise = Promise.resolve(props.dataProvider(dataProviderArgs));
-    dataProviderCache.set(cacheKey, dataPropsPromise);
-
-    // Optional: Clean up cache after some time to prevent memory leaks
-    setTimeout(() => {
-      dataProviderCache.delete(cacheKey);
-    }, 5 * 1000); // 5 seconds
-  }
-
-  const dataProps = await dataPropsPromise;
+  const dataProps = await props.dataProvider(dataProviderArgs);
 
   if (has(dataProps, "$metadata") && isFunction(props.dataProviderMetadataCallback)) {
     props.dataProviderMetadataCallback(props.block, dataProps.$metadata);
