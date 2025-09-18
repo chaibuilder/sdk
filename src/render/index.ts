04d77fc45e4cc4780496a0ba10bc71517ea1badb
Suraj Air <surajair@gmail.com>
2025-09-18 19:08:54 +0530

    feat: export HTML to Chai blocks converter function from render package

--- src/render/index.ts
+++ src/render/index.ts
@@ -4,6 +4,7 @@ export {
   getThemeFontsLinkMarkup,
 } from "@/core/components/canvas/static/chai-theme-helpers";
 export { applyChaiDataBinding } from "@/core/components/canvas/static/new-blocks-render-helpers";
+export { getBlocksFromHTML as convertHTMLToChaiBlocks } from "@/core/import-html/html-to-json";
 export { convertToBlocks, getMergedPartialBlocks } from "@/render/functions";
 export { getStylesForBlocks } from "@/render/get-tailwind-css";
 export { RenderChaiBlocks } from "@/render/render-chai-blocks";
