e35041abde17b690438e25ace1046634258f4acf
Suraj Air <surajair@gmail.com>
2025-09-24 15:08:18 +0530

    fix: swap expand/collapse icons and reposition zoom controls in canvas top bar

--- src/core/components/canvas/topbar/canvas-top-bar.tsx
+++ src/core/components/canvas/topbar/canvas-top-bar.tsx
@@ -17,9 +17,14 @@ const CanvasTopBar: React.FC = () => {
   const [zoom] = useCanvasZoom();
 
   return (
-    <div className="flex h-10 items-center justify-between border-b border-border bg-background/70 px-2">
+    <div className="flex h-10 items-center justify-between border-b border-border bg-background/70 px-2 shadow-xl">
       <div className="flex h-full space-x-2">
         <Breakpoints canvas openDelay={400} />
+        <Separator orientation="vertical" />
+        <div className="flex w-12 cursor-not-allowed items-center justify-center gap-x-1 space-x-0 font-medium text-gray-400">
+          <ZoomInIcon className="h-3.5 w-3.5 flex-shrink-0" />{" "}
+          <div className="text-xs leading-3">{round(zoom, 0)}%</div>
+        </div>
         <Separator orientation="vertical" />
         {darkModeSupport ? (
           <>
@@ -27,11 +32,7 @@ const CanvasTopBar: React.FC = () => {
             <Separator orientation="vertical" />
           </>
         ) : null}
-        <div className="flex w-12 cursor-not-allowed items-center justify-center gap-x-1 space-x-0 font-medium text-gray-400">
-          <ZoomInIcon className="h-3.5 w-3.5 flex-shrink-0" />{" "}
-          <div className="text-xs leading-3">{round(zoom, 0)}%</div>
-        </div>
-        <Separator orientation="vertical" />
+
         <UndoRedo />
         <DataBinding />
       </div>
