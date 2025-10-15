268308d0caa133ae1e16f3539040d0bc28e1f53f
Suraj Air <surajair@gmail.com>
2025-10-15 11:59:26 +0530

    style: reduce dark mode toggle width from 40px to 32px

--- src/core/components/canvas/topbar/dark-mode.tsx
+++ src/core/components/canvas/topbar/dark-mode.tsx
@@ -13,7 +13,7 @@ export function DarkMode() {
         onCheckedChange={() => {
           setDarkMode(!darkMode);
         }}
-        className={`${darkMode ? "bg-violet-600" : "bg-violet-300"} relative ml-2 inline-flex h-[20px] w-[40px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}>
+        className={`${darkMode ? "bg-violet-600" : "bg-violet-300"} relative ml-2 inline-flex h-[20px] w-[32px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}>
         <span
           aria-hidden="true"
           className={`${darkMode ? "translate-x-5" : "translate-x-0"} pointer-events-none -mt-px inline-block h-[18px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
