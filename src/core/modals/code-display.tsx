f8893b422ee2130af3e3275d6155b387707027a8
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2025-11-06 23:51:14 +0530

    feat: added html export option

--- src/core/modals/code-display.tsx
+++ src/core/modals/code-display.tsx
@@ -1,18 +1,32 @@
 import { Button } from "@/ui/shadcn/components/ui/button";
 import Editor from "@monaco-editor/react";
+import { CheckIcon, CopyIcon, DownloadIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 
 interface CodeDisplayProps {
   code: string;
+  language: string;
   onCopy: (code: string) => void;
   onDownload: (code: string) => void;
+  downloadText?: string | React.ReactNode;
 }
 
-export default function CodeDisplay({ code, onCopy, onDownload }: CodeDisplayProps) {
+export default function CodeDisplay({
+  code,
+  onCopy,
+  language = "javascript",
+  onDownload,
+  downloadText = "Download",
+}: CodeDisplayProps) {
+  const [copied, setCopied] = useState(false);
   const [editorCode, setEditorCode] = useState(code);
 
   const handleCopy = () => {
     onCopy(editorCode);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
   };
 
   const handleDownload = () => {
@@ -21,18 +35,18 @@ export default function CodeDisplay({ code, onCopy, onDownload }: CodeDisplayPro
 
   return (
     <div className="flex flex-col gap-4">
-      
-      <div className="h-[600px] max-h-full rounded-md border overflow-hidden">
+      <div className="h-[600px] max-h-full overflow-hidden rounded-md border py-2">
         <Editor
           height="100%"
-          defaultLanguage="jsx"
+          defaultLanguage={language}
+          language={language}
           value={editorCode}
           onChange={(value) => setEditorCode(value || "")}
           theme="vs-light"
           options={{
             minimap: { enabled: false },
-            fontSize: 12,
-            lineNumbers: "on",
+            fontSize: 14,
+            lineNumbers: "off",
             roundedSelection: false,
             scrollBeyondLastLine: false,
             automaticLayout: true,
@@ -47,13 +61,13 @@ export default function CodeDisplay({ code, onCopy, onDownload }: CodeDisplayPro
           }}
         />
       </div>
-      
+
       <div className="flex justify-end gap-2">
         <Button type="button" variant="outline" onClick={handleCopy}>
-          Copy
+          {copied ? <CheckIcon className="text-green-500" /> : <CopyIcon />} {copied ? "Copied" : "Copy"}
         </Button>
         <Button type="button" onClick={handleDownload}>
-          Download
+          <DownloadIcon /> {downloadText}
         </Button>
       </div>
     </div>
