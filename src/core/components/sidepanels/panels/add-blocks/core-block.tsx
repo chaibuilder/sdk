e0f5e32483987c702b7ae7e9b77494e0db7fc86f
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-11-07 16:17:57 +0530

    feat: enhance CoreBlock styling with dynamic class names

--- src/core/components/sidepanels/panels/add-blocks/core-block.tsx
+++ src/core/components/sidepanels/panels/add-blocks/core-block.tsx
@@ -7,6 +7,7 @@ import { syncBlocksWithDefaults } from "@chaibuilder/runtime";
 import { BoxIcon } from "@radix-ui/react-icons";
 import { useFeature } from "flagged";
 import { useAtom } from "jotai";
+import { kebabCase } from "lodash";
 import { capitalize, has, isFunction, omit } from "lodash-es";
 import { createElement } from "react";
 import { useTranslation } from "react-i18next";
@@ -58,7 +59,9 @@ export const CoreBlock = ({
             }}
             draggable={dnd ? "true" : "false"}
             className={
-              "cursor-pointer space-y-2 rounded-lg border border-border p-3 text-center hover:bg-slate-300/50 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400 dark:border-gray-700 dark:text-white dark:hover:bg-slate-800/50 dark:disabled:bg-gray-900 dark:disabled:text-foreground"
+              `${kebabCase(`chai-block-${type}`)} cursor-pointer space-y-2 rounded-lg border border-border p-3 text-center hover:bg-slate-300/50 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400 dark:border-gray-700 dark:text-white dark:hover:bg-slate-800/50 dark:disabled:bg-gray-900 dark:disabled:text-foreground ${
+                disabled ? "opacity-50" : ""
+              }`
             }>
             {createElement(icon || BoxIcon, { className: "w-4 h-4 mx-auto" })}
             <p className="truncate text-xs">{capitalize(t(label || type))}</p>
