f4d02318e2213f36cb60fe619ed761253d726459
Suraj Air <surajair@gmail.com>
2026-01-14 12:40:09 +0530

    chore: setup render pages

--- frameworks/nextjs/package/ChaiBuilder.ts
+++ frameworks/nextjs/package/ChaiBuilder.ts
@@ -1,75 +1,189 @@
 import { ChaiBlock } from "@chaibuilder/runtime";
-import { db, safeQuery, schema } from "@chaibuilder/sdk/server";
-import { error } from "console";
-import { and, eq } from "drizzle-orm";
+import { unstable_cache } from "next/cache";
+import { cache } from "react";
+import { getPageStyles } from "./get-page-styles";
+
+//TODO: https://github.com/chaibuilder/frameworks/blob/main/packages/next/src/blocks/rsc/render-chai-blocks.tsx
+
+type ChaiBuilderPage =
+  | {
+      id: string;
+      slug: string;
+      lang: string;
+      name: string;
+      pageType: string;
+      languagePageId: string;
+      blocks: ChaiBlock[];
+    }
+  | { error: string };
 
 class ChaiBuilder {
-  public static draftMode: boolean = false;
-  public static fallbackLang: string = "en";
-  public static appId: string = "";
+  private static appId?: string;
+  private static draftMode: boolean = false;
+  private static fallbackLang: string = "en";
 
-  public static setDraftMode(mode: boolean) {
-    this.draftMode = mode;
-  }
+  static verifyInit() {}
 
-  public static setFallbackLang(lang: string) {
-    this.fallbackLang = lang;
-  }
-
-  public static initialise(appId: string, draftMode: boolean = false) {
-    this.appId = appId;
-    this.draftMode = draftMode;
-    this.getSiteSettings();
-  }
-
-  public static async getSiteSettings() {
-    const table = this.draftMode ? db.query.apps : db.query.appsOnline;
-    const { data: website } = await safeQuery(() =>
-      table.findFirst({
-        where: and(eq(schema.apps.id, this.appId)),
-      }),
-    );
-    if (error) {
-      throw error;
+  static init = async (appId: string, draftMode: boolean = false) => {
+    if (!appId) {
+      throw new Error("Please initialize ChaiBuilder with an API key");
     }
-    this.setFallbackLang(website.fallbackLang);
-    return website;
+    ChaiBuilder.appId = appId;
+    await ChaiBuilder.loadSiteSettings(draftMode);
+  };
+
+  static getAppId() {
+    return ChaiBuilder.appId;
   }
 
-  public static async getPageBySlug(slug: string): Promise<Record<string, unknown>> {
-    //TODO: GetPageBySlug(slug: string)
-    console.log("GetPageBySlug", slug);
-    return {};
+  static setAppId(appId: string) {
+    ChaiBuilder.appId = appId;
   }
 
-  public static async getFullPage(pageId: string): Promise<Record<string, unknown>> {
-    //TODO: GetFullPage(pageId: string)
-    console.log("GetFullPage", pageId);
-    return {};
+  static async loadSiteSettings(draftMode: boolean) {
+    ChaiBuilder.verifyInit();
+    const siteSettings = await ChaiBuilder.getSiteSettings();
+
+    ChaiBuilder?.setFallbackLang(siteSettings?.fallbackLang);
+    ChaiBuilder?.setDraftMode(draftMode);
   }
 
-  public static async getBlocksStyles(blocks: ChaiBlock[]): Promise<string> {
-    //TODO: GetBlocksStyles(blocks: ChaiBlock[])
-    console.log("GetBlocksStyles", blocks);
-    return "";
+  static setDraftMode(draftMode: boolean) {
+    ChaiBuilder.verifyInit();
+    ChaiBuilder.draftMode = draftMode;
   }
 
-  public static async getPageExternalData(args: {
+  static setFallbackLang(lang: string) {
+    ChaiBuilder.verifyInit();
+    ChaiBuilder.fallbackLang = lang;
+  }
+
+  static getFallbackLang() {
+    ChaiBuilder.verifyInit();
+    return ChaiBuilder.fallbackLang;
+  }
+
+  static async getSiteSettings() {
+    ChaiBuilder.verifyInit();
+    return await unstable_cache(async () => /* TODO: implement */ {}, [`website-settings-${ChaiBuilder.getAppId()}`], {
+      tags: [`website-settings`, `website-settings-${ChaiBuilder.getAppId()}`],
+    })();
+  }
+
+  static async getPartialPageBySlug(slug: string) {
+    ChaiBuilder.verifyInit();
+    // if the slug is of format /_partial/{langcode}/{uuid}, get the uuid. langcode is optional
+    const uuid = slug.split("/").pop();
+    if (!uuid) {
+      throw new Error("Invalid slug format for partial page");
+    }
+
+    // Fetch the partial page data
+    const siteSettings = await ChaiBuilder.getSiteSettings();
+    const data = await ChaiBuilder.getFullPage(uuid); //TODO: Replace by ORM https://github.com/chaibuilder/frameworks/blob/main/packages/next/src/server/builder-api/src/ChaiBuilderPages.ts#L843
+    const fallbackLang = siteSettings?.fallbackLang;
+    const lang = slug.split("/").length > 3 ? slug.split("/")[2] : fallbackLang;
+    return { ...data, fallbackLang, lang };
+  }
+
+  static getPage = cache(async (slug: string) => {
+    ChaiBuilder.verifyInit();
+    if (slug.startsWith("/_partial/")) {
+      return await ChaiBuilder.getPartialPageBySlug(slug);
+    }
+    const page: ChaiBuilderPage = await ChaiBuilder.getPageBySlug(slug); //TODO: Replace by ORM https://github.com/chaibuilder/frameworks/blob/main/packages/next/src/server/builder-api/src/ChaiBuilderPages.ts#L686
+    if ("error" in page) {
+      return page;
+    }
+
+    const siteSettings = await ChaiBuilder.getSiteSettings();
+    const tagPageId = page.id;
+    return await unstable_cache(
+      async () => {
+        const data = await ChaiBuilder.pages?.getFullPage(page.languagePageId);
+        const fallbackLang = siteSettings?.fallbackLang;
+        return { ...data, fallbackLang, lang: page.lang || fallbackLang };
+      },
+      ["page-" + page.languagePageId, page.lang, page.id, slug],
+      { tags: ["page-" + tagPageId] },
+    )();
+  });
+
+  static async getPageSeoData(slug: string) {
+    ChaiBuilder.verifyInit();
+    if (slug.startsWith("/_partial/")) {
+      return {
+        title: "Partial Page",
+        description: "This is a partial page.",
+        robots: {
+          index: false,
+          follow: false,
+          googleBot: {
+            index: false,
+            follow: false,
+          },
+        },
+      };
+    }
+    const page = await ChaiBuilder.getPage(slug);
+    if ("error" in page) {
+      return {
+        title: "Page Not Found",
+        description: "The requested page could not be found.",
+        robots: { index: false, follow: false },
+      };
+    }
+
+    const externalData = await ChaiBuilder.getPageExternalData({
+      blocks: page.blocks,
+      pageProps: page,
+      pageType: page.pageType,
+      lang: page.lang,
+    });
+
+    const seo = withDataBinding(page?.seo ?? {}, externalData);
+
+    return {
+      title: seo?.title,
+      description: seo?.description,
+      openGraph: {
+        title: seo?.ogTitle || seo?.title,
+        description: seo?.ogDescription || seo?.description,
+        images: seo?.ogImage ? [seo?.ogImage] : [],
+      },
+      alternates: {
+        canonical: seo?.canonicalUrl || "",
+      },
+      robots: {
+        index: !seo?.noIndex,
+        follow: !seo?.noFollow,
+        googleBot: {
+          index: !seo?.noIndex,
+          follow: !seo?.noFollow,
+        },
+      },
+    };
+  }
+
+  static async getPageExternalData(args: {
     blocks: ChaiBlock[];
-    pageProps: Record<string, any>;
+    pageProps: ChaiPageProps;
     pageType: string;
     lang: string;
-  }): Promise<Record<string, unknown>> {
-    //TODO: GetPageExternalData({ blocks: ChaiBlock[], pageProps: Record<string, any>, pageType: string, lang: string })
-    console.log("GetPageExternalData", args);
-    return {};
+  }) {
+    ChaiBuilder.verifyInit();
+    return await ChaiBuilder.getPageData(args); //TODO: Replace by orm
   }
 
-  public static async resolveLinks(args: { blocks: ChaiBlock[] }): Promise<Partial<ChaiBlock>[]> {
-    //TODO: GetResolveLinks(blocks: ChaiBlock[])
-    console.log("GetResolveLinks", args);
-    return [];
+  static async getPageStyles(blocks: ChaiBlock[]) {
+    ChaiBuilder.verifyInit();
+    return await getPageStyles(blocks);
   }
+
+  static resolvePageLink = cache(async (href: string, lang: string) => {
+    ChaiBuilder.verifyInit();
+    return await ChaiBuilder?.resolvePageLink(href, lang); //TODO: Replace by orm
+  });
 }
 
 export { ChaiBuilder };
