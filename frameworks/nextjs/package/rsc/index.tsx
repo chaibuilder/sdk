74f40dc620eb69984fc5576b186bfca0ed548129
Suraj Air <surajair@gmail.com>
2026-01-16 08:27:16 +0530

    refactor: reorganize NextRenderChaiBlocks exports and move file to rsc directory

--- frameworks/nextjs/package/rsc/index.tsx
+++ frameworks/nextjs/package/rsc/index.tsx
@@ -1,8 +1,8 @@
 export * from "./chai-page-styles";
-export * from "./json-ld";
-export * from "./preview-banner";
-export { RenderChaiBlocks as NextJSRenderChaiBlocks } from "./render-chai-blocks";
 export { ImageBlock } from "./image-block";
-export { LinkBlock } from "./link-block";
+export * from "./json-ld";
 export { JSONLD } from "./json-ld";
-
+export { LinkBlock } from "./link-block";
+export { NextJSRenderChaiBlocks } from "./next-render-chai-blocks";
+export * from "./preview-banner";
+export { RenderChaiBlocks } from "./render-chai-blocks";
