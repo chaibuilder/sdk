fe947f663445b237dc6d62ec100b4669acb72dde
Suraj Air <surajair@gmail.com>
2026-02-02 16:29:02 +0530

    refactor: update home page navigation and add ChaiLibraryConfig export

--- src/core/main/index.ts
+++ src/core/main/index.ts
@@ -57,5 +57,3 @@ export type { ChaiLibrary, ChaiLibraryBlock } from "@/types/chaibuilder-editor-p
 // constants
 export { PERMISSIONS } from "@/core/constants/PERMISSIONS";
 export type { ChaiTheme as ChaiThemeValues } from "@/types/chaibuilder-editor-props";
-
-export const ChaiLibraryConfig = {};
