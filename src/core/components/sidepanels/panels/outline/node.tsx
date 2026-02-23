ce988605a3572dfd0fea686ee4b4b357e6a025fc
Chaibuilder Developer <developers@chaibuilder.com>
2026-02-23 08:48:29 +0530

    refactor: add eslint-disable comment for exhaustive-deps in outline node useEffect

--- src/core/components/sidepanels/panels/outline/node.tsx
+++ src/core/components/sidepanels/panels/outline/node.tsx
@@ -144,6 +144,7 @@ export const Node = memo(({ node, style, dragHandle }: NodeRendererProps<any>) =
     }, 500);
 
     return () => clearTimeout(timedToggle);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [willReceiveDrop, node, isDragging]);
 
   const setDropAttribute = (id: string, value: string) => {
