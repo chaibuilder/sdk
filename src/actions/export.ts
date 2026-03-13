25676f372d00829fe6de723852fcd4aec8db8ede
amitdhiman5086 <amitdhiman5086@gmail.com>
2026-03-13 11:00:18 +0530

    feat: export new action-related classes and types

--- src/actions/export.ts
+++ src/actions/export.ts
@@ -1,8 +1,11 @@
 import ChaiActionsRegistry from "./builder/actions-registery";
 import { getAskAiSystemPrompt } from "./classes/system-prompt";
+import { ActionError } from "./builder/action-error";
+import { ChaiBaseAction } from "./builder/base-action";
+import { ChaiAction, ChaiActionContext } from "./builder/chai-action-interface";
 
 export { initChaiBuilderActionHandler } from "./builder/chai-builder-actions-handler";
 export { PublishChangesAction } from "./builder/publish-changes";
 export { db, safeQuery, schema } from "./db";
 export { LANGUAGES } from "./LANGUAGES";
-export { ChaiActionsRegistry, getAskAiSystemPrompt };
+export { ChaiActionsRegistry, ChaiBaseAction, ActionError, getAskAiSystemPrompt, type ChaiAction, type ChaiActionContext };
